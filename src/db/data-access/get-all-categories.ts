701593fdb88723cf68fd7a1687879b32a2f5be23
DevWedeloper <vicnathangabrielle@gmail.com>
2024-08-13 14:00:12 +0800

    feat: create getAllCategories function

---
+++ src/db/data-access/get-all-categories.ts
@@ -0,0 +1,25 @@
+import { db } from '..';
+import { Categories, categories } from '../schema';
+
+export type CategoryWithSubcategories = Categories & {
+  subcategories: CategoryWithSubcategories[];
+};
+
+export async function getAllCategories() {
+  const categoriesData = await db.select().from(categories);
+
+  const buildCategoryTree = (
+    categories: Categories[],
+    parentId: number | null = null,
+  ): CategoryWithSubcategories[] => {
+    return categories
+      .filter((category) => category.parentCategoryId === parentId)
+      .map((category) => ({
+        ...category,
+        subcategories: buildCategoryTree(categories, category.id),
+      }));
+  };
+
+  const categoryTree = buildCategoryTree(categoriesData);
+  return categoryTree;
+}
