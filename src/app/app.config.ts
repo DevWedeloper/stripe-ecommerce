0fa4a05931df4bed01f89b31c7025eb5be49b44a
DevWedeloper <vicnathangabrielle@gmail.com>
2024-08-20 15:36:35 +0800

    feat: enable scrollPositionRestoration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,19 @@ import { provideFileRouter } from '@analogjs/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
+import { withInMemoryScrolling } from '@angular/router';
 import { provideTrpcClient } from 'src/trpc-client';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideFileRouter(),
+    provideFileRouter(
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+      }),
+    ),
     provideHttpClient(withFetch()),
     provideClientHydration(),
     provideTrpcClient(),
   ],
 };
-  
