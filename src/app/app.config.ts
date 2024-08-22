cedd10ca73a50854cb0030076692150691bf4505
DevWedeloper <vicnathangabrielle@gmail.com>
2024-08-22 11:01:22 +0800

    feat: enable withComponentInputBinding

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,17 @@ import { provideFileRouter } from '@analogjs/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
-import { withInMemoryScrolling } from '@angular/router';
+import {
+  withComponentInputBinding,
+  withInMemoryScrolling,
+} from '@angular/router';
 import { provideTrpcClient } from 'src/trpc-client';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideFileRouter(
+      withComponentInputBinding(),
       withInMemoryScrolling({
         scrollPositionRestoration: 'enabled',
       }),
