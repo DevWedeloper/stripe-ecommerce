832936889c7bb32af0d9c11ca97fd4d6f8a525cb
DevWedeloper <vicnathangabrielle@gmail.com>
2024-08-28 11:25:29 +0800

    feat: create getProductById

---
+++ src/db/data-access/get-product-by-id.ts
@@ -0,0 +1,12 @@
+import { eq } from 'drizzle-orm';
+import { db } from '..';
+import { Products, products } from '../schema';
+
+export const getProductById = async (id: number): Promise<Products | null> => {
+  const [product] = await db
+    .select()
+    .from(products)
+    .where(eq(products.id, id))
+    .limit(1);
+  return product || null;
+};
