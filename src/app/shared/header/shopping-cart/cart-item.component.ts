7a63340e319fafb40e74307453949194793ab678
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-09 12:08:05 +0800

    feat: create cart-item component

---
+++ src/app/shared/header/shopping-cart/cart-item.component.ts
@@ -0,0 +1,56 @@
+import { CurrencyPipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+import { provideIcons } from '@ng-icons/core';
+import { lucideTrash2 } from '@ng-icons/lucide';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+import { ProductWithQuantity } from '../../shopping-cart.service';
+import { getS3ImageUrl } from '../../utils';
+
+@Component({
+  selector: 'app-cart-item',
+  standalone: true,
+  imports: [CurrencyPipe, HlmButtonDirective, HlmIconComponent],
+  providers: [provideIcons({ lucideTrash2 })],
+  host: {
+    class: 'mb-2 block',
+  },
+  template: `
+    <div class="mb-2 flex items-center">
+      <img
+        [src]="getS3ImageUrl(product().imagePath)"
+        [alt]="product().name"
+        class="mr-2 h-16 w-16 rounded-md object-cover"
+      />
+      <div class="flex-1">
+        <h4 class="text-lg font-semibold">{{ product().name }}</h4>
+        <p class="text-sm">{{ product().description }}</p>
+        <p class="text-sm font-semibold">
+          {{ product().price | currency: product().currency }} x
+          {{ product().quantity }}
+        </p>
+      </div>
+    </div>
+    <button
+      hlmBtn
+      variant="destructive"
+      class="w-full"
+      (click)="removeFromCartChange.emit(product().id)"
+    >
+      <hlm-icon size="sm" class="mr-2" name="lucideTrash2" />
+      Remove
+    </button>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CartItemComponent {
+  product = input.required<ProductWithQuantity>();
+  removeFromCartChange = output<number>();
+
+  protected getS3ImageUrl = getS3ImageUrl;
+}
