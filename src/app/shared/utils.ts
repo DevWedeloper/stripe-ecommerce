f12eb19816cc7f7830f9d32e9f2890cbcdd1b636
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-10 17:49:42 +0800

    feat: create setQueryParams

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -1,6 +1,22 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
 export const getS3ImageUrl = (imagePath: string | null) => {
   const s3Url = environment.s3Url;
   return `${s3Url}/${imagePath}`;
+};
+
+export const setQueryParams = (
+  queryParams: {
+    [key: string]: string | number;
+  },
+  route: ActivatedRoute,
+  router: Router,
+): void => {
+  router.navigate([], {
+    relativeTo: route,
+    queryParams,
+    queryParamsHandling: 'merge',
+    replaceUrl: true,
+  });
 };
