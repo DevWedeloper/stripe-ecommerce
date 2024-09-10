ebd6d70f8d20178dcbc7c8b07aad6043a0ba0247
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-10 17:29:37 +0800

    refactor: create product folder

--- src/server/trpc/routers/products.ts
+++ src/server/trpc/routers/products.ts
@@ -1,6 +1,6 @@
 import { positiveIntSchema } from 'src/schemas/zod-schemas';
-import { getProductById } from 'src/server/use-cases/get-product-by-id';
-import { getProductsByCategoryName } from 'src/server/use-cases/get-products-by-category-name';
+import { getProductById } from 'src/server/use-cases/product/get-product-by-id';
+import { getProductsByCategoryName } from 'src/server/use-cases/product/get-products-by-category-name';
 import { z } from 'zod';
 import { publicProcedure, router } from '../trpc';
 
