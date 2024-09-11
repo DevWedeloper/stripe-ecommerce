42bbe213172038fae2fd70153e2583075550dea3
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-11 12:41:07 +0800

    refactor: remove setQueryParams

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -1,25 +1,9 @@
-import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { positiveIntSchema } from 'src/schemas/zod-schemas';
 
 export const getS3ImageUrl = (imagePath: string | null) => {
   const s3Url = environment.s3Url;
   return `${s3Url}/${imagePath}`;
-};
-
-export const setQueryParams = (
-  queryParams: {
-    [key: string]: string | number;
-  },
-  route: ActivatedRoute,
-  router: Router,
-): void => {
-  router.navigate([], {
-    relativeTo: route,
-    queryParams,
-    queryParamsHandling: 'merge',
-    replaceUrl: true,
-  });
 };
 
 export const parseToPositiveInt = (value: any, fallback: number): number => {
