5bce6e860c1bd07cfe670f6e4926530896b3e639
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-12 15:54:35 +0800

    refactor: rename products -> product

--- src/db/data-access/utils.ts
+++ src/db/data-access/utils.ts
@@ -5,14 +5,14 @@ import { Products } from '../schema';
 export const lower = (col: Column) => sql<string>`lower(${col})`;
 
 type ProductWithTotalCount = {
-  product: Products;
+  products: Products;
   totalCount: number;
 };
 
 export const formatPaginatedResult = (
   productArray: ProductWithTotalCount[],
 ): { products: Products[]; totalProducts: number } => {
-  const products = productArray.map((result) => result.product);
+  const products = productArray.map((result) => result.products);
   const totalProducts = productArray[0]?.totalCount || 0;
 
   return { products, totalProducts };
