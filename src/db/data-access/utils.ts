39e0d1af2032824cabb9bf8b13c8347afeeba864
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-12 15:52:35 +0800

    feat: create formatPaginatedResult

--- src/db/data-access/utils.ts
+++ src/db/data-access/utils.ts
@@ -1,4 +1,19 @@
 import type { Column } from 'drizzle-orm';
 import { sql } from 'drizzle-orm';
+import { Products } from '../schema';
 
 export const lower = (col: Column) => sql<string>`lower(${col})`;
+
+type ProductWithTotalCount = {
+  product: Products;
+  totalCount: number;
+};
+
+export const formatPaginatedResult = (
+  productArray: ProductWithTotalCount[],
+): { products: Products[]; totalProducts: number } => {
+  const products = productArray.map((result) => result.product);
+  const totalProducts = productArray[0]?.totalCount || 0;
+
+  return { products, totalProducts };
+};
