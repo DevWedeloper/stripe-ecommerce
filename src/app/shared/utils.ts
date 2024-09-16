d382c3a54325816664a8992278d585126c181583
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-16 22:40:20 +0800

    refactor: remove export of getS3ImageUrl

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -5,7 +5,7 @@ import { environment } from 'src/environments/environment';
 import { positiveIntSchema } from 'src/schemas/zod-schemas';
 import { PaginatedProducts } from 'src/server/use-cases/types/paginated-products.type';
 
-export const getS3ImageUrl = (imagePath: string | null): string => {
+const getS3ImageUrl = (imagePath: string | null): string => {
   const s3Url = environment.s3Url;
   return `${s3Url}/${imagePath}`;
 };
