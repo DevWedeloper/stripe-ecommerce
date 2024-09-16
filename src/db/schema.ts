5eac3f6e06e3d18ece92f7e8da9739a970cb917a
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-16 17:22:26 +0800

    feat: create gin search_index

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,21 +1,34 @@
-import { InferSelectModel } from 'drizzle-orm';
+import { InferSelectModel, sql } from 'drizzle-orm';
 import {
   AnyPgColumn,
+  index,
   integer,
   pgTable,
   text,
   unique,
 } from 'drizzle-orm/pg-core';
 
-export const products = pgTable('products', {
-  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
-  name: text('name').notNull(),
-  description: text('description').notNull(),
-  price: integer('price').notNull(),
-  currency: text('currency').notNull(),
-  imagePath: text('image_path'),
-  stock: integer('stock').notNull(),
-});
+export const products = pgTable(
+  'products',
+  {
+    id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+    name: text('name').notNull(),
+    description: text('description').notNull(),
+    price: integer('price').notNull(),
+    currency: text('currency').notNull(),
+    imagePath: text('image_path'),
+    stock: integer('stock').notNull(),
+  },
+  (table) => ({
+    searchIndex: index('search_index').using(
+      'gin',
+      sql`(
+        setweight(to_tsvector('english', ${table.name}), 'A') ||
+        setweight(to_tsvector('english', ${table.description}), 'B')
+      )`,
+    ),
+  }),
+);
 
 export const categories = pgTable('categories', {
   id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
