e168f0fc1098016aa3efdab15ed3f02003dbd15e
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-17 14:16:49 +0800

    feat: create product detail page

---
+++ src/app/pages/products/[productId].page.ts
@@ -0,0 +1,98 @@
+import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmCardDirective } from '@spartan-ng/ui-card-helm';
+import { ProductDetailService } from 'src/app/pages/products/product-detail.service';
+import { QuantitySelectorComponent } from 'src/app/shared/quantity-selector.component';
+import { ShoppingCartService } from 'src/app/shared/shopping-cart.service';
+import { Products } from 'src/db/schema';
+import { ProductDetailSkeletonComponent } from './product-detail-skeleton.component';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [
+    CurrencyPipe,
+    NgOptimizedImage,
+    HlmButtonDirective,
+    HlmCardDirective,
+    QuantitySelectorComponent,
+    ProductDetailSkeletonComponent,
+  ],
+  providers: [ProductDetailService],
+  template: `
+    @if (!isLoading()) {
+      @if (product()) {
+        <div
+          hlmCard
+          class="grid animate-fade-in grid-cols-1 gap-8 p-4 md:grid-cols-2"
+        >
+          <div class="relative h-[600px] w-full rounded-lg">
+            <img
+              [ngSrc]="product()!.imagePath!"
+              [alt]="product()!.name"
+              class="object-cover"
+              [placeholder]="product()!.placeholder!"
+              fill
+            />
+          </div>
+
+          <div class="flex flex-col justify-center">
+            <h1 class="mb-4 text-3xl font-bold">
+              {{ product()!.name }}
+            </h1>
+            <p class="mb-4">{{ product()!.description }}</p>
+            <div class="mb-4 text-xl font-semibold">
+              Price:
+              {{ product()!.price | currency: product()!.currency }}
+            </div>
+            <div class="mb-4">
+              Stock:
+              {{ product()!.stock > 0 ? product()!.stock : 'Out of stock' }}
+            </div>
+
+            <app-quantity-selector
+              [(quantity)]="quantity"
+              [stock]="product()!.stock"
+              class="mb-4"
+            />
+
+            <button
+              hlmBtn
+              [disabled]="product()!.stock <= 0"
+              (click)="addToCart(product()!)"
+            >
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      } @else {
+        <div>Product not found...</div>
+      }
+    } @else {
+      <app-product-detail-skeleton />
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class ProductDetailPageComponent {
+  private productDetailService = inject(ProductDetailService);
+  private shoppingCartService = inject(ShoppingCartService);
+
+  protected quantity = signal(1);
+
+  protected product = this.productDetailService.product;
+  protected isLoading = this.productDetailService.isLoading;
+
+  protected addToCart(product: Products): void {
+    this.shoppingCartService.addToCart({
+      ...product,
+      quantity: this.quantity(),
+    });
+  }
+}
