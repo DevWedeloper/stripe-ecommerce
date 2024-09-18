c0f84e8072164a552e81b3690a2103c150d6b533
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-18 18:05:07 +0800

    refactor: change ProductWithTotalCount expected shape

--- src/db/data-access/utils.ts
+++ src/db/data-access/utils.ts
@@ -4,15 +4,34 @@ import { Products } from '../schema';
 
 export const lower = (col: Column) => sql<string>`lower(${col})`;
 
-type ProductWithTotalCount = {
-  products: Products;
+type ProductWithTotalCount = Products & {
   totalCount: number;
 };
 
 export const formatPaginatedResult = (
   productArray: ProductWithTotalCount[],
 ): { products: Products[]; totalProducts: number } => {
-  const products = productArray.map((result) => result.products);
+  const products = productArray.map(
+    ({
+      id,
+      name,
+      description,
+      price,
+      currency,
+      imagePath,
+      placeholder,
+      stock,
+    }) => ({
+      id,
+      name,
+      description,
+      price,
+      currency,
+      imagePath,
+      placeholder,
+      stock,
+    }),
+  );
   const totalProducts = productArray[0]?.totalCount || 0;
 
   return { products, totalProducts };
