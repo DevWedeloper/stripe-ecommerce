9cf5718dd3dd730159ed0d43810fa3a4e0e82dd4
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-20 01:07:47 +0800

    perf: improve ranking by using string_agg

--- src/db/data-access/product/get-paginated-products-by-keyword.ts
+++ src/db/data-access/product/get-paginated-products-by-keyword.ts
@@ -11,46 +11,25 @@ export const getPaginatedProductsByKeyword = async (
   const matchQuery = sql`
     setweight(to_tsvector('english', ${products.name}), 'A') ||
     setweight(to_tsvector('english', ${products.description}), 'B') ||
-    setweight(to_tsvector('english', ${tags.name}), 'C')
+    setweight(to_tsvector('english', string_agg(${tags.name}, ' ')), 'C')
   `;
 
-  const sq = db
-    .selectDistinctOn([products.id], {
+  const productsResult = await db
+    .select({
       ...getTableColumns(products),
-      rank: sql<number>`ts_rank(${matchQuery}, websearch_to_tsquery('english', ${keyword}))`.as(
-        'rank',
-      ),
-      rankCd:
-        sql<number>`ts_rank_cd(${matchQuery}, websearch_to_tsquery('english', ${keyword}))`.as(
-          'rank_cd',
-        ),
+      rank: sql<number>`ts_rank(${matchQuery}, websearch_to_tsquery('english', ${keyword}))`,
+      rankCd: sql<number>`ts_rank_cd(${matchQuery}, websearch_to_tsquery('english', ${keyword}))`,
+      totalCount,
     })
     .from(products)
+    .groupBy(products.id)
     .leftJoin(productTags, eq(products.id, productTags.productId))
     .leftJoin(tags, eq(productTags.tagId, tags.id))
-    .where(
+    .having(
       sql`(
         ${matchQuery} @@ websearch_to_tsquery('english', ${keyword})
       )`,
     )
-    .orderBy((t) => [t.id, desc(t.rank)])
-    .as('sq');
-
-  const productsResult = await db
-    .select({
-      id: sq.id,
-      name: sq.name,
-      description: sq.description,
-      price: sq.price,
-      currency: sq.currency,
-      imagePath: sq.imagePath,
-      placeholder: sq.placeholder,
-      stock: sq.stock,
-      rank: sq.rank,
-      rankCd: sq.rankCd,
-      totalCount,
-    })
-    .from(sq)
     .orderBy((t) => desc(t.rank))
     .offset(offset)
     .limit(pageSize);
