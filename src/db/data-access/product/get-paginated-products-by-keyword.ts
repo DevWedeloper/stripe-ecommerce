c594dfe3da4edb61cfb092d53de8a497828ccf4d
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-19 19:20:57 +0800

    perf: improve query speed

--- src/db/data-access/product/get-paginated-products-by-keyword.ts
+++ src/db/data-access/product/get-paginated-products-by-keyword.ts
@@ -1,5 +1,4 @@
 import { desc, eq, getTableColumns, sql } from 'drizzle-orm';
-import { union } from 'drizzle-orm/pg-core';
 import { db } from 'src/db';
 import { Products, productTags, products, tags } from 'src/db/schema';
 import { formatPaginatedResult, totalCount } from '../utils';
@@ -9,69 +8,55 @@ export const getPaginatedProductsByKeyword = async (
   offset: number,
   pageSize: number,
 ): Promise<{ products: Products[]; totalProducts: number }> => {
-  const productMatchQuery = sql`(
+  const matchQuery = sql`
     setweight(to_tsvector('english', ${products.name}), 'A') ||
-    setweight(to_tsvector('english', ${products.description}), 'B')), websearch_to_tsquery('english', ${keyword})`;
+    setweight(to_tsvector('english', ${products.description}), 'B') ||
+    setweight(to_tsvector('english', ${tags.name}), 'C')
+  `;
 
-  const tagMatchQuery = sql`to_tsvector('english', ${tags.name}), websearch_to_tsquery('english', ${keyword})`;
-
-  const productQuery = db
-    .select({
+  const sq = db
+    .selectDistinctOn([products.id], {
       ...getTableColumns(products),
-      rank: sql<number>`ts_rank(${productMatchQuery})`.as('rank'),
-      rankCd: sql<number>`ts_rank_cd(${productMatchQuery})`.as('rankCd'),
+      rank: sql<number>`ts_rank(${matchQuery}, websearch_to_tsquery('english', ${keyword}))`.as(
+        'rank',
+      ),
+      rankCd:
+        sql<number>`ts_rank_cd(${matchQuery}, websearch_to_tsquery('english', ${keyword}))`.as(
+          'rank_cd',
+        ),
     })
     .from(products)
+    .leftJoin(productTags, eq(products.id, productTags.productId))
+    .leftJoin(tags, eq(productTags.tagId, tags.id))
     .where(
       sql`(
-        setweight(to_tsvector('english', ${products.name}), 'A') ||
-        setweight(to_tsvector('english', ${products.description}), 'B'))
-        @@ websearch_to_tsquery('english', ${keyword}
+        ${matchQuery} @@ websearch_to_tsquery('english', ${keyword})
       )`,
-    );
+    )
+    .orderBy((t) => [t.id, desc(t.rank)])
+    .as('sq');
 
-  const tagQuery = db
+  const productsResult = await db
     .select({
-      ...getTableColumns(products),
-      rank: sql<number>`ts_rank(${tagMatchQuery})`.as('rank'),
-      rankCd: sql<number>`ts_rank_cd(${tagMatchQuery})`.as('rankCd'),
-    })
-    .from(products)
-    .innerJoin(productTags, eq(products.id, productTags.productId))
-    .innerJoin(tags, eq(productTags.tagId, tags.id))
-    .where(
-      sql`to_tsvector('english', ${tags.name}) @@ websearch_to_tsquery('english', ${keyword})`,
-    );
-
-  const combinedQuery = union(productQuery, tagQuery).as('combined_query');
-
-  const distinctIdQuery = db
-    .selectDistinctOn([combinedQuery.id])
-    .from(combinedQuery)
-    .orderBy((t) => t.id)
-    .as('distinct_id_query');
-
-  const result = await db
-    .select({
-      id: distinctIdQuery.id,
-      name: distinctIdQuery.name,
-      description: distinctIdQuery.description,
-      price: distinctIdQuery.price,
-      currency: distinctIdQuery.currency,
-      imagePath: distinctIdQuery.imagePath,
-      placeholder: distinctIdQuery.placeholder,
-      stock: distinctIdQuery.stock,
-      rank: distinctIdQuery.rank,
-      rankCd: distinctIdQuery.rankCd,
+      id: sq.id,
+      name: sq.name,
+      description: sq.description,
+      price: sq.price,
+      currency: sq.currency,
+      imagePath: sq.imagePath,
+      placeholder: sq.placeholder,
+      stock: sq.stock,
+      rank: sq.rank,
+      rankCd: sq.rankCd,
       totalCount,
     })
-    .from(distinctIdQuery)
+    .from(sq)
     .orderBy((t) => desc(t.rank))
     .offset(offset)
     .limit(pageSize);
 
   const { products: productsArray, totalProducts } =
-    formatPaginatedResult(result);
+    formatPaginatedResult(productsResult);
 
   return {
     products: productsArray,
