eee1a383caf2bcbba8c44095e970e991a46782d4
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-20 21:38:28 +0800

    feat: add navigateBack option

--- src/app/shared/go-back-button.component.ts
+++ src/app/shared/go-back-button.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { Location } from '@angular/common';
+import {
+  booleanAttribute,
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { provideIcons } from '@ng-icons/core';
 import { lucideMoveLeft } from '@ng-icons/lucide';
@@ -11,14 +18,28 @@ import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
   imports: [RouterLink, HlmButtonDirective, HlmIconComponent],
   providers: [provideIcons({ lucideMoveLeft })],
   template: `
-    <a hlmBtn variant="ghost" size="lg" class="mb-2" [routerLink]="path()">
-      <hlm-icon size="sm" class="mr-2" name="lucideMoveLeft" />
-      {{ text() }}
-    </a>
+    @if (navigateBack()) {
+      <button hlmBtn variant="ghost" size="lg" class="mb-2" (click)="goBack()">
+        <hlm-icon size="sm" class="mr-2" name="lucideMoveLeft" />
+        {{ text() }}
+      </button>
+    } @else {
+      <a hlmBtn variant="ghost" size="lg" class="mb-2" [routerLink]="path()">
+        <hlm-icon size="sm" class="mr-2" name="lucideMoveLeft" />
+        {{ text() }}
+      </a>
+    }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GoBackButtonComponent {
-  path = input.required<string>();
+  private location = inject(Location);
+
+  path = input<string>();
   text = input.required<string>();
+  navigateBack = input(false, { transform: booleanAttribute });
+
+  protected goBack(): void {
+    this.location.back();
+  }
 }
