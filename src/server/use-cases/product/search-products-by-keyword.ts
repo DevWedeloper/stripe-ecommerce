8f130082ebc797544c1ba17f5b3ef9e5475c2848
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-21 23:15:36 +0800

    feat: create searchProductsByKeyword

---
+++ src/server/use-cases/product/search-products-by-keyword.ts
@@ -0,0 +1,26 @@
+import { getPaginatedProductsByKeyword } from 'src/db/data-access/product/get-paginated-products-by-keyword';
+import { PaginatedProducts } from '../types/paginated-products.type';
+
+export const searchProductsByKeyword = async (
+  keyword: string,
+  page: number = 1,
+  pageSize: number = 10,
+): Promise<PaginatedProducts> => {
+  const offset = (page - 1) * pageSize;
+
+  const { products, totalProducts } = await getPaginatedProductsByKeyword(
+    keyword.trim(),
+    offset,
+    pageSize,
+  );
+
+  const totalPages = Math.ceil(totalProducts / pageSize);
+
+  return {
+    page,
+    pageSize,
+    totalPages,
+    totalProducts,
+    products,
+  };
+};
