76282cadaabfca2eef51d94bfac137296033c080
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-22 15:43:59 +0800

    feat: create searchByKeyword route

--- src/server/trpc/routers/products.ts
+++ src/server/trpc/routers/products.ts
@@ -1,6 +1,7 @@
 import { positiveIntSchema } from 'src/schemas/zod-schemas';
 import { getProductById } from 'src/server/use-cases/product/get-product-by-id';
 import { getProductsByCategoryName } from 'src/server/use-cases/product/get-products-by-category-name';
+import { searchProductsByKeyword } from 'src/server/use-cases/product/search-products-by-keyword';
 import { z } from 'zod';
 import { publicProcedure, router } from '../trpc';
 
@@ -19,5 +20,17 @@ export const productsRouter = router({
     .query(
       async ({ input: { name, page, pageSize } }) =>
         await getProductsByCategoryName(name, page, pageSize),
+    ),
+  searchByKeyword: publicProcedure
+    .input(
+      z.object({
+        keyword: z.string(),
+        page: positiveIntSchema,
+        pageSize: positiveIntSchema,
+      }),
+    )
+    .query(
+      async ({ input: { keyword, page, pageSize } }) =>
+        await searchProductsByKeyword(keyword, page, pageSize),
     ),
 });
