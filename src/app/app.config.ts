dae0bc18f5625ab87dfe1cc4a6f8034e10ec3ae1
DevWedeloper <vicnathangabrielle@gmail.com>
2024-09-26 17:21:52 +0800

    feat: use provideRouter

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,16 +3,19 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import {
+  provideRouter,
   withComponentInputBinding,
   withInMemoryScrolling,
 } from '@angular/router';
 import { provideNgxStripe } from 'ngx-stripe';
 import { environment } from 'src/environments/environment';
 import { provideTrpcClient } from 'src/trpc-client';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
     provideFileRouter(
       withComponentInputBinding(),
       withInMemoryScrolling({
