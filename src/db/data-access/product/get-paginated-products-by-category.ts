d76fc96a82d620124e3eeb48f5cdc1c1c4cf763c
DevWedeloper <vicnathangabrielle@gmail.com>
2024-10-02 01:07:48 +0800

    refactor: include imagePath, placeholder

--- src/db/data-access/product/get-paginated-products-by-category.ts
+++ src/db/data-access/product/get-paginated-products-by-category.ts
@@ -1,24 +1,34 @@
-import { eq, getTableColumns, inArray } from 'drizzle-orm';
+import { and, eq, getTableColumns, inArray } from 'drizzle-orm';
+import { ProductsWithThumbnail } from 'src/db/types';
 import { db } from '../..';
-import { Products, productCategories, products } from '../../schema';
+import { productCategories, productImages, products } from '../../schema';
 import { formatPaginatedResult, totalCount } from '../utils';
 
 export const getPaginatedProductsByCategory = async (
   categoryIds: number[],
   offset: number,
   pageSize: number,
-): Promise<{ products: Products[]; totalProducts: number }> => {
+): Promise<{ products: ProductsWithThumbnail[]; totalProducts: number }> => {
   const productsResult = await db
     .selectDistinct({
       ...getTableColumns(products),
+      imagePath: productImages.imagePath,
+      placeholder: productImages.placeholder,
       totalCount,
     })
     .from(products)
+    .leftJoin(
+      productImages,
+      and(
+        eq(productImages.productId, products.id),
+        eq(productImages.isThumbnail, true),
+      ),
+    )
     .innerJoin(productCategories, eq(products.id, productCategories.productId))
     .where(inArray(productCategories.categoryId, categoryIds))
     .offset(offset)
     .limit(pageSize);
-
+  
   const { products: productsArray, totalProducts } =
     formatPaginatedResult(productsResult);
 
