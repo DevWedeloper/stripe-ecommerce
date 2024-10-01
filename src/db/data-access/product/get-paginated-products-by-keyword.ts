8f87b9ce6dd1cf2d6835f1f59200beb9e22b4a9f
DevWedeloper <vicnathangabrielle@gmail.com>
2024-10-02 01:07:42 +0800

    refactor: include imagePath, placeholder

--- src/db/data-access/product/get-paginated-products-by-keyword.ts
+++ src/db/data-access/product/get-paginated-products-by-keyword.ts
@@ -1,13 +1,14 @@
-import { desc, eq, getTableColumns, sql } from 'drizzle-orm';
+import { and, desc, eq, getTableColumns, sql } from 'drizzle-orm';
 import { db } from 'src/db';
-import { Products, productTags, products, tags } from 'src/db/schema';
+import { productImages, productTags, products, tags } from 'src/db/schema';
+import { ProductsWithThumbnail } from 'src/db/types';
 import { formatPaginatedResult, totalCount } from '../utils';
 
 export const getPaginatedProductsByKeyword = async (
   keyword: string,
   offset: number,
   pageSize: number,
-): Promise<{ products: Products[]; totalProducts: number }> => {
+): Promise<{ products: ProductsWithThumbnail[]; totalProducts: number }> => {
   const matchQuery = sql`
     setweight(to_tsvector('english', ${products.name}), 'A') ||
     setweight(to_tsvector('english', ${products.description}), 'B') ||
@@ -17,14 +18,23 @@ export const getPaginatedProductsByKeyword = async (
   const productsResult = await db
     .select({
       ...getTableColumns(products),
+      imagePath: productImages.imagePath,
+      placeholder: productImages.placeholder,
       rank: sql<number>`ts_rank(${matchQuery}, websearch_to_tsquery('english', ${keyword}))`,
       rankCd: sql<number>`ts_rank_cd(${matchQuery}, websearch_to_tsquery('english', ${keyword}))`,
       totalCount,
     })
     .from(products)
+    .leftJoin(
+      productImages,
+      and(
+        eq(productImages.productId, products.id),
+        eq(productImages.isThumbnail, true),
+      ),
+    )
     .leftJoin(productTags, eq(products.id, productTags.productId))
     .leftJoin(tags, eq(productTags.tagId, tags.id))
-    .groupBy(products.id)
+    .groupBy(products.id, productImages.imagePath, productImages.placeholder)
     .having(
       sql`(
         ${matchQuery} @@ websearch_to_tsquery('english', ${keyword})
