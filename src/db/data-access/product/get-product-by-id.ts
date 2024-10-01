c082d6230cd8c2c8cbc7ad6803bda3a4532fae30
DevWedeloper <vicnathangabrielle@gmail.com>
2024-10-02 01:07:17 +0800

    refactor: include imagePath, placeholder, imageObjects

--- src/db/data-access/product/get-product-by-id.ts
+++ src/db/data-access/product/get-product-by-id.ts
@@ -1,12 +1,29 @@
-import { eq } from 'drizzle-orm';
+import { Column, eq, getTableColumns, sql } from 'drizzle-orm';
+import { ImageObject, ProductsWithAllImages } from 'src/db/types';
 import { db } from '../..';
-import { Products, products } from '../../schema';
+import { productImages, products } from '../../schema';
 
-export const getProductById = async (id: number): Promise<Products | null> => {
+export const getProductById = async (
+  id: number,
+): Promise<ProductsWithAllImages | null> => {
+  const maxQuery = (col: Column) =>
+    sql<string>`
+      max(case when ${productImages.isThumbnail} = true then ${col} end)
+    `;
+
   const [product] = await db
-    .select()
+    .select({
+      ...getTableColumns(products),
+      imagePath: maxQuery(productImages.imagePath),
+      placeholder: maxQuery(productImages.placeholder),
+      imageObjects: sql<
+        ImageObject[]
+      >`array_agg(json_build_object('imagePath', ${productImages.imagePath}, 'placeholder', ${productImages.placeholder}))`,
+    })
     .from(products)
+    .leftJoin(productImages, eq(productImages.productId, products.id))
     .where(eq(products.id, id))
+    .groupBy(products.id)
     .limit(1);
   return product || null;
 };
