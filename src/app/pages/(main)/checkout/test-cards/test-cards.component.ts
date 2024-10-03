edbc26ee10f04e37b18fc2a6bc938fb1cae793c8
DevWedeloper <vicnathangabrielle@gmail.com>
2024-10-04 00:03:59 +0800

    feat: create test cards component

---
+++ src/app/pages/(main)/checkout/test-cards/test-cards.component.ts
@@ -0,0 +1,133 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  viewChild,
+} from '@angular/core';
+import { provideIcons } from '@ng-icons/core';
+import {
+  lucideCheck,
+  lucideChevronsUpDown,
+  lucideCreditCard,
+  lucideEllipsis,
+  lucideShieldCheck,
+  lucideX,
+} from '@ng-icons/lucide';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import {
+  BrnCollapsibleComponent,
+  BrnCollapsibleContentComponent,
+  BrnCollapsibleTriggerDirective,
+} from '@spartan-ng/ui-collapsible-brain';
+import { hlm } from '@spartan-ng/ui-core';
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+import { CardCopyButtonComponent } from './card-copy-button.component';
+
+@Component({
+  selector: 'app-test-cards',
+  standalone: true,
+  imports: [
+    HlmButtonDirective,
+    HlmIconComponent,
+    BrnCollapsibleComponent,
+    BrnCollapsibleTriggerDirective,
+    BrnCollapsibleContentComponent,
+    CardCopyButtonComponent,
+  ],
+  providers: [
+    provideIcons({
+      lucideCreditCard,
+      lucideChevronsUpDown,
+      lucideCheck,
+      lucideShieldCheck,
+      lucideX,
+      lucideEllipsis,
+    }),
+  ],
+  template: `
+    <brn-collapsible
+      class="flex w-[350px] max-w-[85vw] flex-col rounded-t-md border border-border bg-background p-4"
+    >
+      <div class="flex items-center justify-between">
+        <h4 class="flex items-center gap-2 text-sm font-semibold">
+          <hlm-icon size="sm" name="lucideCreditCard" />
+          TEST CARDS
+        </h4>
+        <button brnCollapsibleTrigger hlmBtn variant="ghost" size="icon">
+          <hlm-icon size="sm" name="lucideChevronsUpDown" />
+          <span class="sr-only">Toggle</span>
+        </button>
+      </div>
+      <brn-collapsible-content [class]="computedClass()">
+        <div class="overflow-hidden">
+          <div class="flex flex-col gap-2">
+            <app-card-copy-button [cardNumber]="cardNumbers.success">
+              <span class="flex items-center gap-2">
+                <hlm-icon size="sm" name="lucideCheck" />
+                Success
+              </span>
+              <span class="flex items-center gap-2">
+                <hlm-icon size="sm" name="lucideEllipsis" />
+                4242
+              </span>
+            </app-card-copy-button>
+            <app-card-copy-button [cardNumber]="cardNumbers.authentication">
+              <span class="flex items-center gap-2">
+                <hlm-icon size="sm" name="lucideShieldCheck" />
+                Authentication
+              </span>
+              <span class="flex items-center gap-2">
+                <hlm-icon size="sm" name="lucideEllipsis" />
+                3155
+              </span>
+            </app-card-copy-button>
+            <app-card-copy-button [cardNumber]="cardNumbers.decline">
+              <span class="flex items-center gap-2">
+                <hlm-icon size="sm" name="lucideX" />
+                Decline
+              </span>
+              <span class="flex items-center gap-2">
+                <hlm-icon size="sm" name="lucideEllipsis" />
+                0002
+              </span>
+            </app-card-copy-button>
+          </div>
+
+          <p class="mt-4">
+            Click to copy the card number or use any of
+            <a
+              hlmBtn
+              variant="link"
+              class="h-6 px-0.5 text-base underline"
+              href="https://docs.stripe.com/testing#cards"
+              target="_blank"
+            >
+              Stripe's test cards
+            </a>
+            . Use any future expiration date and three-number CVC.
+          </p>
+        </div>
+      </brn-collapsible-content>
+    </brn-collapsible>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TestCardsComponent {
+  private brnCollapsibleTrigger = viewChild.required(
+    BrnCollapsibleTriggerDirective,
+  );
+
+  private state = computed(() => this.brnCollapsibleTrigger().state());
+
+  protected cardNumbers = {
+    success: '4242424242424242',
+    authentication: '4000000000003220',
+    decline: '4000000000000002',
+  };
+
+  protected computedClass = computed(() => {
+    const gridRows =
+      this.state() === 'open' ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]';
+    return hlm('grid transition-all', gridRows);
+  });
+}
