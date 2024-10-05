100a05ed45fdef8c07a15195f89eef4fddee0681
DevWedeloper <vicnathangabrielle@gmail.com>
2024-10-06 02:11:22 +0800

    feat: create product image gallery

---
+++ src/app/pages/(main)/products/product-image-gallery.component.ts
@@ -0,0 +1,41 @@
+import { NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { HlmCarouselImports } from '@spartan-ng/ui-carousel-helm';
+import { ImageObject } from 'src/db/types';
+
+@Component({
+  selector: 'app-product-image-gallery',
+  standalone: true,
+  imports: [NgOptimizedImage, HlmCarouselImports],
+  host: {
+    class: 'block h-full',
+  },
+  template: `
+    <div class="flex w-full items-center justify-center p-8">
+      <hlm-carousel class="w-full">
+        <hlm-carousel-content>
+          @for (imageObject of imageObjects(); track $index) {
+            <hlm-carousel-item>
+              <div class="relative h-[600px] w-full">
+                <img
+                  [ngSrc]="imageObject.imagePath"
+                  [alt]="alt()"
+                  [placeholder]="imageObject.imagePath"
+                  class="rounded-lg object-cover"
+                  fill
+                />
+              </div>
+            </hlm-carousel-item>
+          }
+        </hlm-carousel-content>
+        <button hlm-carousel-previous></button>
+        <button hlm-carousel-next></button>
+      </hlm-carousel>
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductImageGalleryComponent {
+  imageObjects = input.required<ImageObject[]>();
+  alt = input.required<string>();
+}
