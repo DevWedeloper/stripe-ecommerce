061d95e0c6d01d50bb2f76d5f2c2611aed8bda11
DevWedeloper <vicnathangabrielle@gmail.com>
2024-10-15 22:03:59 +0800

    feat: update types

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,4 +1,10 @@
-import { ProductImages, Products } from './schema';
+import {
+  ProductImages,
+  ProductItems,
+  Products,
+  VariationOptions,
+  Variations,
+} from './schema';
 
 type NullishImageObject = {
   [K in keyof Pick<ProductImages, 'imagePath' | 'placeholder'>]:
@@ -6,11 +12,23 @@ type NullishImageObject = {
     | null;
 };
 
-export type ImageObject = Pick<ProductImages, 'imagePath' | 'placeholder'>;
+export type ImageObject = Pick<
+  ProductImages,
+  'id' | 'imagePath' | 'placeholder'
+>;
 
-export type ProductsWithAllImages = Products &
-  NullishImageObject & {
+export type ProductDetails = Products & {
+  productItems: ProductItemObject[];
+} & NullishImageObject & {
     imageObjects: ImageObject[];
-  };
+  } & { variations: Record<string, string[]> };
 
 export type ProductsWithThumbnail = Products & NullishImageObject;
+
+export type ProductItemObject = Pick<
+  ProductItems,
+  'id' | 'sku' | 'stock' | 'price'
+> & { variations: VariationObject[] };
+
+export type VariationObject = Pick<Variations, 'name'> &
+  Pick<VariationOptions, 'value'>;
