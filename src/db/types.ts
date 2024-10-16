9ef315cf5a5f01fba96a2f1c3da4be6e40772a86
DevWedeloper <vicnathangabrielle@gmail.com>
2024-10-16 23:39:52 +0800

    refactor: improve type syntax

--- src/db/types.ts
+++ src/db/types.ts
@@ -6,29 +6,37 @@ import {
   Variations,
 } from './schema';
 
+type Nullable<T> = T | null;
+
 type NullishImageObject = {
-  [K in keyof Pick<ProductImages, 'imagePath' | 'placeholder'>]:
-    | ProductImages[K]
-    | null;
+  imagePath: Nullable<ProductImages['imagePath']>;
+  placeholder: Nullable<ProductImages['placeholder']>;
 };
 
-export type ImageObject = Pick<
-  ProductImages,
-  'id' | 'imagePath' | 'placeholder'
->;
+export type ImageObject = {
+  id: ProductImages['id'];
+  imagePath: ProductImages['imagePath'];
+  placeholder: ProductImages['placeholder'];
+};
 
-export type ProductDetails = Products & {
-  items: ProductItemObject[];
-} & NullishImageObject & {
+export type ProductDetails = Products &
+  NullishImageObject & {
+    items: ProductItemObject[];
     imageObjects: ImageObject[];
-  } & { variations: Record<string, string[]> };
+    variations: Record<string, string[]>;
+  };
 
 export type ProductsWithThumbnail = Products & NullishImageObject;
 
-export type ProductItemObject = Pick<
-  ProductItems,
-  'id' | 'sku' | 'stock' | 'price'
-> & { variations: VariationObject[] };
+export type ProductItemObject = {
+  id: ProductItems['id'];
+  sku: ProductItems['sku'];
+  stock: ProductItems['stock'];
+  price: ProductItems['price'];
+  variations: VariationObject[];
+};
 
-export type VariationObject = Pick<Variations, 'name'> &
-  Pick<VariationOptions, 'value'>;
+export type VariationObject = {
+  name: Variations['name'];
+  value: VariationOptions['value'];
+};
