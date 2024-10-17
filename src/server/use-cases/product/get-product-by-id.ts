d1b61ebceee1d96e89fd59e1078ddc957f407d74
DevWedeloper <vicnathangabrielle@gmail.com>
2024-10-18 00:21:10 +0800

    refactor: change return type and function

--- src/server/use-cases/product/get-product-by-id.ts
+++ src/server/use-cases/product/get-product-by-id.ts
@@ -1,6 +1,6 @@
 import { getProductById as fetchProductById } from 'src/db/data-access/product/get-product-by-id';
-import { ProductsWithAllImages } from 'src/db/types';
+import { ProductDetails } from 'src/db/types';
 
 export const getProductById = async (
   id: number,
-): Promise<ProductsWithAllImages | null> => fetchProductById(id);
+): Promise<ProductDetails | null> => fetchProductById(id);
