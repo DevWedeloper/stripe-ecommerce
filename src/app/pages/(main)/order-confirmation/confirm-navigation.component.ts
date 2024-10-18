8d6ea340c943c04afc91f3dc632bbd051ac9e476
DevWedeloper <vicnathangabrielle@gmail.com>
2024-10-19 05:16:02 +0800

    feat: create confirm navigation component

---
+++ src/app/pages/(main)/order-confirmation/confirm-navigation.component.ts
@@ -0,0 +1,51 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { BrnDialogRef } from '@spartan-ng/ui-dialog-brain';
+import {
+  HlmDialogDescriptionDirective,
+  HlmDialogFooterComponent,
+  HlmDialogHeaderComponent,
+  HlmDialogTitleDirective,
+} from '@spartan-ng/ui-dialog-helm';
+import { ShoppingCartService } from 'src/app/shared/shopping-cart.service';
+
+@Component({
+  selector: 'app-confirm-navigation',
+  standalone: true,
+  imports: [
+    HlmButtonDirective,
+    HlmDialogHeaderComponent,
+    HlmDialogFooterComponent,
+    HlmDialogTitleDirective,
+    HlmDialogDescriptionDirective,
+  ],
+  host: {
+    class: 'w-[425px]',
+  },
+  template: `
+    <hlm-dialog-header>
+      <h3 hlmDialogTitle>Confirm Navigation</h3>
+      <p hlmDialogDescription>
+        Are you sure you want to cancel processing your payment?
+      </p>
+    </hlm-dialog-header>
+    <hlm-dialog-footer class="mt-2">
+      <button hlmBtn (click)="cancel()">Cancel</button>
+      <button hlmBtn (click)="forward()">Forward</button>
+    </hlm-dialog-footer>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ConfirmNavigationComponent {
+  private _dialogRef = inject(BrnDialogRef);
+  private shoppingCartService = inject(ShoppingCartService);
+
+  protected cancel(): void {
+    this._dialogRef.close(false);
+  }
+
+  protected forward(): void {
+    this._dialogRef.close(true);
+    this.shoppingCartService.editable$.next(true);
+  }
+}
