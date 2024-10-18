3686be3ae8e1590ca768c756f31241659cbe739b
DevWedeloper <vicnathangabrielle@gmail.com>
2024-10-19 05:07:08 +0800

    refactor: move object manipulation to use-case

--- src/server/use-cases/product/get-product-by-id.ts
+++ src/server/use-cases/product/get-product-by-id.ts
@@ -3,4 +3,23 @@ import { ProductDetails } from 'src/db/types';
 
 export const getProductById = async (
   id: number,
-): Promise<ProductDetails | null> => fetchProductById(id);
+): Promise<ProductDetails | null> => {
+  const result = await fetchProductById(id);
+
+  if (!result) {
+    return null;
+  }
+
+  const product = {
+    ...result,
+    variations: result.variations.reduce<Record<string, string[]>>(
+      (acc, { name: key, value }) => ({
+        ...acc,
+        [key]: acc[key] ? [...acc[key], value] : [value],
+      }),
+      {},
+    ),
+  };
+
+  return product;
+};
