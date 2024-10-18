e873a51ef46db785dc9b3256cc9f4d45eb940b58
DevWedeloper <vicnathangabrielle@gmail.com>
2024-10-19 05:10:34 +0800

    feat: use item variation component

--- src/app/pages/(main)/shopping-cart/cart-page-items.component.ts
+++ src/app/pages/(main)/shopping-cart/cart-page-items.component.ts
@@ -10,8 +10,9 @@ import { lucideTrash2 } from '@ng-icons/lucide';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { HlmCardDirective } from '@spartan-ng/ui-card-helm';
 import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+import { ItemVariationComponent } from 'src/app/shared/item-variation.component';
 import { QuantitySelectorComponent } from 'src/app/shared/quantity-selector.component';
-import { ProductWithQuantity } from 'src/app/shared/shopping-cart.service';
+import { CartItem } from 'src/app/shared/shopping-cart.service';
 
 @Component({
   selector: 'app-cart-page-items',
@@ -23,38 +24,38 @@ import { ProductWithQuantity } from 'src/app/shared/shopping-cart.service';
     HlmIconComponent,
     HlmCardDirective,
     QuantitySelectorComponent,
+    ItemVariationComponent,
   ],
   providers: [provideIcons({ lucideTrash2 })],
   template: `
     <div hlmCard class="grid grid-cols-4 items-center p-4">
       <div class="relative flex h-16 w-16 justify-center rounded">
         <img
-          [ngSrc]="product().imagePath!"
-          [alt]="product().name"
+          [ngSrc]="item().imagePath!"
+          [alt]="item().name"
           class="object-cover"
-          [placeholder]="product().placeholder!"
+          [placeholder]="item().placeholder!"
           fill
         />
       </div>
 
       <div class="flex flex-col">
-        <h2 class="text-lg font-semibold">{{ product().name }}</h2>
-        <p>{{ product().description }}</p>
+        <h2 class="text-lg font-semibold">{{ item().name }}</h2>
+        <p>{{ item().description }}</p>
+        <app-item-variation [variations]="item().variations" />
       </div>
 
       <div class="flex items-center justify-center gap-2">
         <app-quantity-selector
-          [quantity]="product().quantity"
-          [stock]="product().stock"
+          [quantity]="item().quantity"
+          [stock]="item().stock"
           (quantityChange)="quantityChange.emit($event)"
         />
       </div>
 
       <div class="flex flex-col items-center justify-end gap-4">
         <span class="text-2xl font-semibold">
-          {{
-            product().price * product().quantity | currency: product().currency
-          }}
+          {{ item().price * item().quantity | currency: item().currency }}
         </span>
         <button
           hlmBtn
@@ -72,7 +73,7 @@ import { ProductWithQuantity } from 'src/app/shared/shopping-cart.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CartPageItemsComponent {
-  product = input.required<ProductWithQuantity>();
+  item = input.required<CartItem>();
   isEditable = input.required<boolean>();
   quantityChange = output<number>();
   removeFromCartChange = output<void>();
