e111a0ada153a7fd2fb7e4a0bfeb6d5f1f2cf881
DevWedeloper <vicnathangabrielle@gmail.com>
2024-10-26 07:59:31 +0800

    refactor: add width

--- src/app/shared/fallback-ui/empty-product-lists.component.ts
+++ src/app/shared/fallback-ui/empty-product-lists.component.ts
@@ -5,7 +5,7 @@ import { hlmMuted } from '@spartan-ng/ui-typography-helm';
   selector: 'app-empty-product-lists',
   standalone: true,
   template: `
-    <img src="/empty-product-lists.svg" alt="No products found" />
+    <img src="/empty-product-lists.svg" alt="No products found" class="w-60" />
     <p class="${hlmMuted} mt-2 text-center">No products found...</p>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
