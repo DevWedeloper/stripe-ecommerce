9ecf715f3f18219b25d174df4341bb12da98f96f
DevWedeloper <vicnathangabrielle@gmail.com>
2024-10-27 08:26:14 +0800

    feat: create empty product details component

---
+++ src/app/shared/fallback-ui/empty-product-details.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { hlmMuted } from '@spartan-ng/ui-typography-helm';
+
+@Component({
+  selector: 'app-empty-product-details',
+  standalone: true,
+  template: `
+    <img
+      src="/empty-product-lists.svg"
+      alt="Product not found"
+      class="w-[80vw] max-w-80"
+    />
+    <p class="${hlmMuted} mt-2 text-center">Product not found...</p>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EmptyProductDetailsComponent {}
