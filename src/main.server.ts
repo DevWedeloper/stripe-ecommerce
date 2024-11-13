309b3ed8d43a1c503e912bd983403a66145b0440
DevWedeloper <vicnathangabrielle@gmail.com>
2024-11-14 00:13:54 +0800

    feat: provideServerContext

--- src/main.server.ts
+++ src/main.server.ts
@@ -5,6 +5,9 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { renderApplication } from '@angular/platform-server';
 
+import { provideServerContext } from '@analogjs/router/server';
+import { ServerContext } from '@analogjs/router/tokens';
+
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
@@ -16,10 +19,15 @@ export function bootstrap() {
   return bootstrapApplication(AppComponent, config);
 }
 
-export default async function render(url: string, document: string) {
+export default async function render(
+  url: string,
+  document: string,
+  serverContext: ServerContext,
+) {
   const html = await renderApplication(bootstrap, {
     document,
     url,
+    platformProviders: [provideServerContext(serverContext)],
   });
 
   return html;
