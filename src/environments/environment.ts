b9c6a96b81cace4cdaeb77e2667a4e6243c8e6fe
DevWedeloper <vicnathangabrielle@gmail.com>
2024-11-15 00:08:06 +0800

    refactor: use getEnvVar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
+import { getEnvVar } from 'src/env';
+
 export const environment = {
-  s3Url: import.meta.env['VITE_S3_URL'] as string,
-  stripePublishableKey: import.meta.env[
-    'VITE_STRIPE_PUBLISHABLE_KEY'
-  ] as string,
+  s3Url: getEnvVar('VITE_S3_URL'),
+  stripePublishableKey: getEnvVar('VITE_STRIPE_PUBLISHABLE_KEY'),
 };
