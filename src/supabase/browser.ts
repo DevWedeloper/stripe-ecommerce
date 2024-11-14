512cbb3d5d557d115c3689c997a2b7acdcc6f04d
DevWedeloper <vicnathangabrielle@gmail.com>
2024-11-15 00:07:12 +0800

    refactor: use createBrowserClient and rename file

--- src/supabase/browser.ts
+++ src/supabase/browser.ts
@@ -1,8 +1,8 @@
-import { createClient as supabaseCreateClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr';
 import { getEnvVar } from 'src/env';
 
 const SUPABASE_URL = getEnvVar('VITE_SUPABASE_URL');
 const SUPABASE_ANON_KEY = getEnvVar('VITE_SUPABASE_ANON_KEY');
 
 export const createClient = () =>
-  supabaseCreateClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+  createBrowserClient(SUPABASE_URL, SUPABASE_ANON_KEY);
