f271a9d931a0376d33d3f03d86a4673aa913078e
DevWedeloper <vicnathangabrielle@gmail.com>
2024-11-16 02:10:36 +0800

    feat: add createAdminClient

--- src/supabase/server.ts
+++ src/supabase/server.ts
@@ -1,4 +1,5 @@
 import {
+  CookieOptions,
   createServerClient,
   parseCookieHeader,
   serializeCookieHeader,
@@ -8,21 +9,36 @@ import { getEnvVar } from 'src/env';
 
 const SUPABASE_URL = getEnvVar('VITE_SUPABASE_URL');
 const SUPABASE_ANON_KEY = getEnvVar('VITE_SUPABASE_ANON_KEY');
+const SUPABASE_SERVICE_ROLE_KEY = getEnvVar('SUPABASE_SERVICE_ROLE_KEY');
+
+const cookieHandler = (event: H3Event) => ({
+  getAll() {
+    return parseCookieHeader(event.node.req.headers.cookie ?? '');
+  },
+  setAll(
+    cookiesToSet: {
+      name: string;
+      value: string;
+      options: CookieOptions;
+    }[],
+  ) {
+    cookiesToSet.forEach(({ name, value, options }) => {
+      event.node.res.appendHeader(
+        'Set-Cookie',
+        serializeCookieHeader(name, value, options),
+      );
+    });
+  },
+});
 
 export const createClient = (event: H3Event) => {
   return createServerClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-    cookies: {
-      getAll() {
-        return parseCookieHeader(event.node.req.headers.cookie ?? '');
-      },
-      setAll(cookiesToSet) {
-        cookiesToSet.forEach(({ name, value, options }) => {
-          event.node.res.appendHeader(
-            'Set-Cookie',
-            serializeCookieHeader(name, value, options),
-          );
-        });
-      },
-    },
+    cookies: cookieHandler(event),
+  });
+};
+
+export const createAdminClient = (event: H3Event) => {
+  return createServerClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY, {
+    cookies: cookieHandler(event),
   });
 };
