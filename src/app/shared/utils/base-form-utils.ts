5269de1a6aeefc813a6bdad19396b943a7f8a440
DevWedeloper <vicnathangabrielle@gmail.com>
2024-11-20 00:04:42 +0800

    feat: create base form utils

---
+++ src/app/shared/utils/base-form-utils.ts
@@ -0,0 +1,50 @@
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmFormFieldComponent } from '@spartan-ng/ui-formfield-helm';
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+import { createProvider } from 'src/app/shared/create-provider';
+import { DynamicValidatorMessageDirective } from 'src/app/shared/dynamic-form-errors/dynamic-validator-message.directive';
+import { ERROR_COMPONENT } from 'src/app/shared/dynamic-form-errors/input-error/error-component.token';
+import { AuthInputErrorComponent } from '../auth-input-error.component';
+import {
+  hasDigit,
+  hasLowerCase,
+  hasSpecialChar,
+  hasUpperCase,
+  zodEmail,
+} from '../validators';
+
+export const sharedFormDeps = [
+  ReactiveFormsModule,
+  HlmLabelDirective,
+  HlmInputDirective,
+  HlmFormFieldComponent,
+  HlmButtonDirective,
+  HlmIconComponent,
+  DynamicValidatorMessageDirective,
+];
+
+export const authInputErrorProvider = createProvider(ERROR_COMPONENT, {
+  useValue: AuthInputErrorComponent,
+});
+
+export const emailField = { email: ['', [Validators.required, zodEmail]] };
+export const passwordField = { password: ['', [Validators.required]] };
+export const passwordWithValidationField = {
+  password: [
+    '',
+    [
+      Validators.required,
+      Validators.minLength(8),
+      hasUpperCase,
+      hasLowerCase,
+      hasDigit,
+      hasSpecialChar,
+    ],
+  ],
+};
+export const confirmPasswordField = {
+  confirmPassword: ['', [Validators.required]],
+};
