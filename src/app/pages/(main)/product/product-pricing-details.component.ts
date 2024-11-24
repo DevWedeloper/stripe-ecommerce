a1b8c0698f37078217d87b53e9209504a4a4f764
DevWedeloper <vicnathangabrielle@gmail.com>
2024-11-25 04:06:08 +0800

    feat: create product pricing details component

---
+++ src/app/pages/(main)/product/product-pricing-details.component.ts
@@ -0,0 +1,52 @@
+import { CurrencyPipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  model,
+  output,
+} from '@angular/core';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { QuantitySelectorComponent } from 'src/app/shared/quantity-selector.component';
+
+@Component({
+  selector: 'app-product-pricing-details',
+  standalone: true,
+  imports: [CurrencyPipe, HlmButtonDirective, QuantitySelectorComponent],
+  template: `
+    <div class="mb-4 text-xl font-semibold">
+      Price:
+      {{ price() | currency: currency() }}
+    </div>
+    <div class="mb-4">
+      Stock:
+      {{ stock() > 0 ? stock() : 'Out of stock' }}
+    </div>
+
+    <app-quantity-selector
+      [(quantity)]="quantity"
+      [stock]="stock()"
+      class="mb-4"
+    />
+
+    <button
+      hlmBtn
+      [disabled]="stock() <= 0 || quantity() <= 0"
+      (click)="onClick()"
+    >
+      Add to Cart
+    </button>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductPricingDetailsComponent {
+  stock = input.required<number>();
+  price = input.required<number>();
+  currency = input.required<string>();
+  quantity = model.required<number>();
+  addToCart = output<void>();
+
+  protected onClick(): void {
+    this.addToCart.emit();
+  }
+}
