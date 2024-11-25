9099d46ecce93324323cf284cd289b15f8ebfe31
DevWedeloper <vicnathangabrielle@gmail.com>
2024-11-25 08:01:27 +0800

    refactor: change import path

--- src/app/shared/header/account/sign-out.service.ts
+++ src/app/shared/header/account/sign-out.service.ts
@@ -2,7 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Router } from '@angular/router';
 import { filter, map, materialize, merge, share } from 'rxjs';
-import { AuthService } from '../../auth.service';
+import { AuthService } from '../../data-access/auth.service';
 import { errorStream, successStream } from '../../utils/rxjs';
 import { showError } from '../../utils/toast';
 
