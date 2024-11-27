e42a3fcc32dd1297c2b66b75eaceaa75a8cb69c8
DevWedeloper <vicnathangabrielle@gmail.com>
2024-11-28 04:35:41 +0800

    feat: create sign-up page

---
+++ src/app/pages/(auth)/sign-up/(sign-up).page.ts
@@ -0,0 +1,114 @@
+import { RouteMeta } from '@analogjs/router';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  viewChild,
+} from '@angular/core';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { FormBuilder, ValueChangeEvent } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmCardDirective } from '@spartan-ng/ui-card-helm';
+import { filter } from 'rxjs';
+import {
+  confirmPasswordField,
+  emailField,
+  passwordWithValidationField,
+} from 'src/app/shared/utils/form';
+import { disableTemporarilyStream } from 'src/app/shared/utils/rxjs';
+import { passwordShouldMatch } from 'src/app/shared/validators';
+import { isNotAuthenticatedGuard } from '../../../shared/guards/is-not-authenticated.guard';
+import { SignUpService } from './data-access/sign-up.service';
+import { SignUpFormComponent } from './ui/sign-up-form.component';
+
+export const routeMeta: RouteMeta = {
+  canActivate: [isNotAuthenticatedGuard],
+};
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [
+    RouterLink,
+    HlmButtonDirective,
+    HlmCardDirective,
+    SignUpFormComponent,
+  ],
+  host: {
+    class: 'flex flex-col',
+  },
+  template: `
+    <div hlmCard class="flex flex-col gap-4 rounded-lg border p-4">
+      <h1 class="text-center text-lg font-bold">Sign-Up</h1>
+      <app-sign-up-form
+        [form]="form"
+        [isLoading]="isLoading()"
+        [disableTemporarily]="disableTemporarily()"
+        (submitChange)="onSubmit()"
+      />
+    </div>
+    <div class="flex justify-between">
+      <p>
+        Go back to
+        <a hlmBtn variant="link" class="h-auto p-0 text-base" routerLink="/">
+          Home
+        </a>
+      </p>
+      <p>
+        Have an account?
+        <a
+          hlmBtn
+          variant="link"
+          class="h-auto p-0 text-base"
+          routerLink="/login"
+        >
+          Login
+        </a>
+      </p>
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class SignUpPageComponent {
+  private fb = inject(FormBuilder);
+  private signUpService = inject(SignUpService);
+
+  private success$ = this.signUpService.signUpSuccessWithData$;
+  protected isLoading = this.signUpService.isLoading;
+  private error$ = this.signUpService.error$;
+
+  private signUpForm = viewChild.required(SignUpFormComponent);
+
+  protected form = this.fb.nonNullable.group(
+    {
+      ...emailField,
+      ...passwordWithValidationField,
+      ...confirmPasswordField,
+    },
+    {
+      validators: [passwordShouldMatch],
+    },
+  );
+
+  private disableTemporarily$ = disableTemporarilyStream({
+    enable: this.form.events.pipe(
+      filter((event) => event instanceof ValueChangeEvent),
+    ),
+    disable: this.error$,
+  });
+
+  protected disableTemporarily = toSignal(this.disableTemporarily$, {
+    initialValue: false,
+  });
+
+  constructor() {
+    this.success$
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.signUpForm().formDir().resetForm());
+  }
+
+  protected onSubmit(): void {
+    this.signUpService.signUp(this.form.getRawValue());
+  }
+}
