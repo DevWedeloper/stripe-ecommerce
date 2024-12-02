a0178cc27d1f21e06a91dcf0a1f56a120a9d35f1
DevWedeloper <vicnathangabrielle@gmail.com>
2024-12-02 11:00:26 +0800

    feat: create createAddress procedure

--- src/server/trpc/routers/address.ts
+++ src/server/trpc/routers/address.ts
@@ -1,3 +1,10 @@
-import { router } from '../trpc';
+import { createInsertSchema } from 'drizzle-zod';
+import { addresses } from 'src/db/schema';
+import { createAddress } from 'src/server/use-cases/address/create-address';
+import { publicProcedure, router } from '../trpc';
 
-export const addressRouter = router({});
+export const addressRouter = router({
+  createAddress: publicProcedure
+    .input(createInsertSchema(addresses))
+    .mutation(async ({ input }) => await createAddress(input)),
+});
