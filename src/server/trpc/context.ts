fc05567c598eb0f3e5f7bc7009f74097ee4299b3
DevWedeloper <vicnathangabrielle@gmail.com>
2024-12-02 10:52:44 +0800

    feat: expose event on context

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,7 +1,11 @@
 import { inferAsyncReturnType } from '@trpc/server';
+import { H3Event } from 'h3';
+
 /**
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
  */
-export const createContext = () => ({});
+export const createContext = async (event: H3Event) => ({
+  event,
+});
 export type Context = inferAsyncReturnType<typeof createContext>;
