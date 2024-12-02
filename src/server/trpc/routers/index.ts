63c3d0b3ffdb72a0f0ade8a715e76eb69fc55fce
DevWedeloper <vicnathangabrielle@gmail.com>
2024-12-02 10:54:49 +0800

    refactor: add addressRouter

--- src/server/trpc/routers/index.ts
+++ src/server/trpc/routers/index.ts
@@ -1,10 +1,12 @@
 import { router } from '../trpc';
+import { addressRouter } from './address';
 import { authRouter } from './auth';
 import { categoriesRouter } from './categories';
 import { productsRouter } from './products';
 import { stripeRouter } from './stripe';
 
 export const appRouter = router({
+  address: addressRouter,
   products: productsRouter,
   categories: categoriesRouter,
   stripe: stripeRouter,
