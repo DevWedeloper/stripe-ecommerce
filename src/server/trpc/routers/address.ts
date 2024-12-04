4ff1540e67f45aa073e440336db88d0b32f36eed
DevWedeloper <vicnathangabrielle@gmail.com>
2024-12-04 08:37:12 +0800

    refactor: change schema to use countryCode

--- src/server/trpc/routers/address.ts
+++ src/server/trpc/routers/address.ts
@@ -1,10 +1,21 @@
 import { createInsertSchema } from 'drizzle-zod';
 import { addresses } from 'src/db/schema';
 import { createAddress } from 'src/server/use-cases/address/create-address';
+import { z } from 'zod';
 import { publicProcedure, router } from '../trpc';
 
 export const addressRouter = router({
   createAddress: publicProcedure
-    .input(createInsertSchema(addresses))
+    .input(
+      createInsertSchema(addresses)
+        .omit({ countryId: true })
+        .merge(
+          z.object({
+            countryCode: z.string().refine((val) => val.length === length, {
+              message: `String must be exactly ${length} characters long`,
+            }),
+          }),
+        ),
+    )
     .mutation(async ({ input }) => await createAddress(input)),
 });
