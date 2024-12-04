69231a3372d79c8e98704e96594cd2fa3032dc50
DevWedeloper <vicnathangabrielle@gmail.com>
2024-12-04 08:36:39 +0800

    refactor: create CTE to find countryId from countryCode

--- src/db/data-access/address/create-address.ts
+++ src/db/data-access/address/create-address.ts
@@ -1,14 +1,33 @@
+import { eq } from 'drizzle-orm';
 import { db } from 'src/db';
-import { addresses, AddressInsert } from 'src/db/schema';
+import { addresses, countries } from 'src/db/schema';
+import { AddressInsertWithCountryCode } from 'src/db/types';
 
 export const createAddress = async (
-  data: AddressInsert,
+  data: AddressInsertWithCountryCode,
 ): Promise<{
   id: number;
 }> => {
+  const countryIdQuery = db.$with('country_id').as(
+    db
+      .select({
+        id: countries.id,
+      })
+      .from(countries)
+      .where(eq(countries.code, data.countryCode)),
+  );
+
   const [address] = await db
+    .with(countryIdQuery)
     .insert(addresses)
-    .values(data)
+    .values({
+      addressLine1: data.addressLine1,
+      addressLine2: data.addressLine2,
+      city: data.city,
+      state: data.state,
+      postalCode: data.postalCode,
+      countryId: Number(countryIdQuery.id),
+    })
     .returning({ id: addresses.id });
 
   return address;
