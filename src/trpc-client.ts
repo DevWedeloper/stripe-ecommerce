1584d239d55f3e41ae28cd5539e6ce8591c2c7c2
DevWedeloper <vicnathangabrielle@gmail.com>
2024-12-10 19:46:45 +0800

    feat: expose TrpcHeaders

--- src/trpc-client.ts
+++ src/trpc-client.ts
@@ -2,9 +2,10 @@ import { createTrpcClient } from '@analogjs/trpc';
 import { inject } from '@angular/core';
 import { AppRouter } from './server/trpc/routers';
 
-export const { provideTrpcClient, TrpcClient } = createTrpcClient<AppRouter>({
-  url: '/api/trpc',
-});
+export const { provideTrpcClient, TrpcClient, TrpcHeaders } =
+  createTrpcClient<AppRouter>({
+    url: '/api/trpc',
+  });
 
 export function injectTrpcClient() {
   return inject(TrpcClient);
