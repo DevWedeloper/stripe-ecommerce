2c768c7f6206f42a207a95c26b7545fcc1d1e473
DevWedeloper <vicnathangabrielle@gmail.com>
2024-12-11 12:37:18 +0800

    feat: create forgot password form

---
+++ src/app/pages/(auth)/forgot-password/ui/forgot-password-form.component.ts
@@ -0,0 +1,54 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { sharedFormDeps, authInputErrorProvider } from 'src/app/shared/utils/form';
+
+@Component({
+  selector: 'app-forgot-password-form',
+  standalone: true,
+  imports: [...sharedFormDeps],
+  providers: [authInputErrorProvider],
+  template: `
+    <form
+      [formGroup]="form()"
+      (ngSubmit)="onSubmit()"
+      class="flex flex-col gap-4"
+    >
+      <hlm-form-field>
+        <label hlmLabel for="email">Email</label>
+        <input
+          hlmInput
+          formControlName="email"
+          id="email"
+          type="email"
+          placeholder="Email"
+          class="w-full"
+        />
+      </hlm-form-field>
+
+      <button
+        hlmBtnWithLoading
+        [disabled]="form().invalid || isLoading() || disableTemporarily()"
+        class="w-full"
+        [isLoading]="isLoading()"
+      >
+        Submit
+      </button>
+    </form>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ForgotPasswordFormComponent {
+  form = input.required<FormGroup>();
+  isLoading = input.required<boolean>();
+  disableTemporarily = input.required<boolean>();
+  submitChange = output<void>();
+
+  protected onSubmit(): void {
+    this.submitChange.emit();
+  }
+}
