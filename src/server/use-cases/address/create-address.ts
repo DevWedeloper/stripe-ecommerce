c368a75527fd01ef5b2845118244008e5e1d01f9
DevWedeloper <vicnathangabrielle@gmail.com>
2024-12-16 19:14:06 +0800

    refactor: change return to void

--- src/server/use-cases/address/create-address.ts
+++ src/server/use-cases/address/create-address.ts
@@ -1,24 +1,7 @@
-import postgres from 'postgres';
 import { findOrCreateAddress } from 'src/db/data-access/address/find-or-create-address';
 import { FindOrCreateAddressData } from 'src/db/types';
 
 export const createAddress = async (
   userId: string,
   data: FindOrCreateAddressData,
-): Promise<{ error: { message: string } | null }> => {
-  try {
-    await findOrCreateAddress(userId, data);
-  } catch (error) {
-    if (error instanceof postgres.PostgresError && error.code === '23505') {
-      return {
-        error: {
-          message:
-            'An address with this information already exists for this user.',
-        },
-      };
-    }
-
-    throw error;
-  }
-  return { error: null };
-};
+) => findOrCreateAddress(userId, data);
