dfdc71d80557ee81367f05094a86728576fa88ef
DevWedeloper <vicnathangabrielle@gmail.com>
2024-12-20 23:58:54 +0800

    refactor: match new API

--- src/db/data-access/address/find-or-create-address.ts
+++ src/db/data-access/address/find-or-create-address.ts
@@ -7,16 +7,9 @@ export const findOrCreateAddress = async (
   data: FindOrCreateAddressData,
 ): Promise<void> => {
   const query = sql`
-    with country_id as (
-      select id
-      from countries
-      where code = ${data.countryCode}
-    ),
-
-    created_address as (
+    with created_address as (
       insert into addresses (address_line1, address_line2, city, state, postal_code, country_id)
-      select ${data.addressLine1}, ${data.addressLine2}, ${data.city}, ${data.state}, ${data.postalCode}, c.id
-      from country_id c
+      select ${data.addressLine1}, ${data.addressLine2}, ${data.city}, ${data.state}, ${data.postalCode}, ${data.countryId}
       on conflict (address_line1, address_line2, city, state, postal_code, country_id) do nothing
       returning id
     ),
@@ -28,12 +21,12 @@ export const findOrCreateAddress = async (
 
       select a.id
       from addresses a
-      join country_id c on a.country_id = c.id 
       where address_line1 = ${data.addressLine1}
         and address_line2 = ${data.addressLine2}
         and city = ${data.city}
         and state = ${data.state}
         and postal_code = ${data.postalCode}
+        and country_id = ${data.countryId}
     ),
 
     receiver_id as (
