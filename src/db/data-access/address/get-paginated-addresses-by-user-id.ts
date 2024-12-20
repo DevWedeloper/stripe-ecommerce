aabd55acdd63bea3595e7f0a6b140f680519538f
DevWedeloper <vicnathangabrielle@gmail.com>
2024-12-20 18:51:39 +0800

    feat: create getPaginatedAddressByUserId

---
+++ src/db/data-access/address/get-paginated-addresses-by-user-id.ts
@@ -0,0 +1,46 @@
+import { desc, eq } from 'drizzle-orm';
+import { db } from 'src/db';
+import { addresses, countries, receivers, userAddresses } from 'src/db/schema';
+import { AddressAndReceiverData } from 'src/db/types';
+import { formatPaginatedResult, totalCount } from '../utils';
+
+export const getPaginatedAddressesByUserId = async (
+  userId: string,
+  offset: number,
+  pageSize: number,
+): Promise<{
+  addresses: AddressAndReceiverData[];
+  totalAddresses: number;
+}> => {
+  const query = db
+    .select({
+      isDefault: userAddresses.isDefault,
+      addressId: addresses.id,
+      addressLine1: addresses.addressLine1,
+      addressLine2: addresses.addressLine2,
+      city: addresses.city,
+      state: addresses.state,
+      postalCode: addresses.postalCode,
+      countryCode: countries.code,
+      receiverId: receivers.id,
+      fullName: receivers.fullName,
+      totalCount: totalCount,
+    })
+    .from(userAddresses)
+    .innerJoin(addresses, eq(addresses.id, userAddresses.addressId))
+    .innerJoin(receivers, eq(receivers.id, userAddresses.receiverId))
+    .innerJoin(countries, eq(countries.id, addresses.countryId))
+    .where(eq(userAddresses.userId, userId))
+    .orderBy(desc(userAddresses.isDefault))
+    .offset(offset)
+    .limit(pageSize);
+
+  const result = await query;
+
+  const { data, totalCount: totalAddresses } = formatPaginatedResult(result);
+
+  return {
+    addresses: data,
+    totalAddresses,
+  };
+};
