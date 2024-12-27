a4cd1b5db3d618a5a284e88c8f59e3b8e387dea7
DevWedeloper <vicnathangabrielle@gmail.com>
2024-12-27 21:22:32 +0800

    feat: add countriesRouter

--- src/server/trpc/routers/index.ts
+++ src/server/trpc/routers/index.ts
@@ -2,10 +2,12 @@ import { router } from '../trpc';
 import { addressRouter } from './address';
 import { authRouter } from './auth';
 import { categoriesRouter } from './categories';
+import { countriesRouter } from './countries';
 import { productsRouter } from './products';
 import { stripeRouter } from './stripe';
 
 export const appRouter = router({
+  countries: countriesRouter,
   address: addressRouter,
   products: productsRouter,
   categories: categoriesRouter,
