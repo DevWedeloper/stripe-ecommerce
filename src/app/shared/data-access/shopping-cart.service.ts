a48a723e34813be4028cc825d9b817920648b445
DevWedeloper <vicnathangabrielle@gmail.com>
2024-12-28 21:48:35 +0800

    feat: persist cart on refresh

--- src/app/shared/data-access/shopping-cart.service.ts
+++ src/app/shared/data-access/shopping-cart.service.ts
@@ -1,7 +1,15 @@
-import { computed, Injectable, signal } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  computed,
+  inject,
+  Injectable,
+  PLATFORM_ID,
+  signal,
+} from '@angular/core';
 import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { Subject } from 'rxjs';
 import { ProductItems } from 'src/db/schema';
+import { z } from 'zod';
 import { CartItem } from '../types/cart';
 import { showError } from '../utils/toast';
 
@@ -10,10 +18,38 @@ type UniqueItemIdentifier = {
   sku: ProductItems['sku'];
 };
 
+const variationObjectSchema = z.object({
+  name: z.string(),
+  value: z.string(),
+  order: z.number().nullable().optional(),
+});
+
+const cartItemSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  currency: z.string().length(3),
+  sku: z.string(),
+  stock: z.number().nonnegative(),
+  price: z.number().nonnegative(),
+  imagePath: z.string().nullable(),
+  placeholder: z.string().nullable(),
+  variations: z.array(variationObjectSchema),
+  productItemId: z.number(),
+  quantity: z.number().min(1),
+});
+
+const cartSchema = z.array(cartItemSchema);
+
 @Injectable({
   providedIn: 'root',
 })
 export class ShoppingCartService {
+  private PLATFORM_ID = inject(PLATFORM_ID);
+
+  private CART_STORAGE_KEY = 'shoppingCart';
+  private CART_EXPIRY_KEY = 'cartExpiry';
+  private CART_EXPIRY_DURATION = 1 * 24 * 60 * 60 * 1000;
+
   private cart = signal<CartItem[]>([]);
 
   private editable$ = new Subject<boolean>();
@@ -41,6 +77,10 @@ export class ShoppingCartService {
   private error$ = new Subject<string>();
 
   constructor() {
+    if (isPlatformBrowser(this.PLATFORM_ID)) {
+      this.loadCartFromStorage();
+    }
+
     this.error$
       .pipe(takeUntilDestroyed())
       .subscribe((error) => showError(error));
@@ -52,6 +92,7 @@ export class ShoppingCartService {
     const cartItem = this.cart().find((i) => isSameItem(i, item));
     if (!cartItem) {
       this.cart.update((cart) => [...cart, item]);
+      this.saveCartToStorage();
       return;
     }
 
@@ -71,6 +112,7 @@ export class ShoppingCartService {
           : p,
       ),
     );
+    this.saveCartToStorage();
   }
 
   removeFromCart(productItemId: number, sku: string): void {
@@ -81,6 +123,8 @@ export class ShoppingCartService {
         (item) => item.productItemId !== productItemId && item.sku !== sku,
       ),
     );
+
+    this.saveCartToStorage();
   }
 
   updateQuantity(productItemId: number, sku: string, quantity: number): void {
@@ -102,6 +146,8 @@ export class ShoppingCartService {
         isSameItem(product, identifier) ? { ...product, quantity } : product,
       ),
     );
+
+    this.saveCartToStorage();
   }
 
   setEditable(editable: boolean): void {
@@ -110,6 +156,59 @@ export class ShoppingCartService {
 
   clearCart(): void {
     this.cart.set([]);
+    this.clearCartStorage();
+  }
+
+  private loadCartFromStorage(): void {
+    const cartData = localStorage.getItem(this.CART_STORAGE_KEY);
+    const cartExpiry = localStorage.getItem(this.CART_EXPIRY_KEY);
+
+    if (!cartData || !cartExpiry) {
+      this.clearCartStorage();
+      return;
+    }
+
+    const parsedCart = (() => {
+      try {
+        return JSON.parse(cartData);
+      } catch {
+        console.error('Malformed cart JSON data.');
+        this.clearCartStorage();
+        return null;
+      }
+    })();
+
+    if (parsedCart === null) {
+      return;
+    }
+
+    const { data, error } = cartSchema.safeParse(parsedCart);
+
+    if (error) {
+      this.clearCartStorage();
+      return;
+    }
+
+    const expiryTime = parseInt(cartExpiry, 10);
+    const currentTime = Date.now();
+
+    if (currentTime <= expiryTime) {
+      this.cart.set(parsedCart);
+      return;
+    }
+
+    this.clearCartStorage();
+  }
+
+  private saveCartToStorage(): void {
+    localStorage.setItem(this.CART_STORAGE_KEY, JSON.stringify(this.cart()));
+    const expiryTime = Date.now() + this.CART_EXPIRY_DURATION;
+    localStorage.setItem(this.CART_EXPIRY_KEY, expiryTime.toString());
+  }
+
+  private clearCartStorage(): void {
+    localStorage.removeItem(this.CART_STORAGE_KEY);
+    localStorage.removeItem(this.CART_EXPIRY_KEY);
   }
 }
 
