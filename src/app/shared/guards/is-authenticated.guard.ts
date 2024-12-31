8570190516916b75917dfe01cb85f3274d31d341
DevWedeloper <vicnathangabrielle@gmail.com>
2024-12-31 18:22:06 +0800

    feat: create isAuthenticated guard

---
+++ src/app/shared/guards/is-authenticated.guard.ts
@@ -0,0 +1,13 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { map } from 'rxjs';
+import { AuthService } from '../data-access/auth.service';
+
+export const isAuthenticatedGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  return authService.user$.pipe(
+    map((user) => (user ? true : router.parseUrl('/login'))),
+  );
+};
