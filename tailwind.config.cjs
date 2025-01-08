d9f27521d5e0dd64d5b0fffb2cf7025ea75e3eaf
DevWedeloper <vicnathangabrielle@gmail.com>
2025-01-09 02:21:42 +0800

    feat: create slide in from left/right animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,9 +13,19 @@ module.exports = {
           from: { opacity: '0' },
           to: { opacity: '1' },
         },
+        'slide-in-from-left': {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        'slide-in-from-right': {
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
       },
       animation: {
         'fade-in': 'fade-in 150ms ease-in-out',
+        'slide-in-from-left': 'slide-in-from-left 500ms ease-in-out',
+        'slide-in-from-right': 'slide-in-from-right 500ms ease-in-out',
       },
     },
   },
