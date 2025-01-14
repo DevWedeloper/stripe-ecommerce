2c62388b57c8fc1ffb727791784c126d39f73e75
DevWedeloper <vicnathangabrielle@gmail.com>
2025-01-15 00:57:19 +0800

    feat: create address list component

---
+++ src/app/pages/account/(account)/address/feature/address-list.component.ts
@@ -0,0 +1,88 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { HlmDialogService } from '@spartan-ng/ui-dialog-helm';
+import { HlmSpinnerComponent } from '@spartan-ng/ui-spinner-helm';
+import { SelectedAddressService } from 'src/app/shared/data-access/address/selected-address.service';
+import { AddressAndReceiverData } from 'src/db/types';
+import { DeleteAddressService } from '../data-access/delete-address.service';
+import { GetAddressService } from '../data-access/get-address.service';
+import { SetAsDefaultAddressService } from '../data-access/set-as-default-address.service';
+import { AddressCardComponent } from '../ui/address-card.component';
+import { UpdateAddressComponent } from './update-address.component';
+
+@Component({
+  selector: 'app-address-list',
+  imports: [AddressCardComponent, HlmSpinnerComponent],
+  host: {
+    class: 'flex flex-col gap-4',
+  },
+  template: `
+    @if (!isInitialLoading()) {
+      @for (address of addresses(); track $index) {
+        <app-address-card
+          [address]="address"
+          [isSetDefaultLoading]="
+            isSelectedSetAsDefaultLoading(address.addressId, address.receiverId)
+          "
+          [isDeleteLoading]="
+            isSelectedDeleteLoading(address.addressId, address.receiverId)
+          "
+          (editChange)="onEdit(address)"
+          (setAsDefaultChange)="
+            onSetAsDefault(address.addressId, address.receiverId)
+          "
+          (deleteChange)="onDelete(address.addressId, address.receiverId)"
+        />
+      } @empty {
+        <p class="text-center">No addresses found</p>
+      }
+    } @else {
+      <div class="flex items-center justify-center">
+        <hlm-spinner />
+      </div>
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AddressListComponent {
+  private _hlmDialogService = inject(HlmDialogService);
+  private getAddressService = inject(GetAddressService);
+  private setAsDefaultAddressService = inject(SetAsDefaultAddressService);
+  private deleteAddressService = inject(DeleteAddressService);
+  private selectedAddressService = inject(SelectedAddressService);
+
+  protected addresses = this.getAddressService.addresses;
+
+  protected isInitialLoading = this.getAddressService.isInitialLoading;
+
+  protected onEdit(data: AddressAndReceiverData): void {
+    this.selectedAddressService.setSelectedAddress(data);
+    this._hlmDialogService.open(UpdateAddressComponent, {
+      contentClass: 'flex',
+    });
+  }
+
+  protected onSetAsDefault(addressId: number, receiverId: number): void {
+    this.setAsDefaultAddressService.setAsDefault(addressId, receiverId);
+  }
+
+  protected onDelete(addressId: number, receiverId: number): void {
+    this.deleteAddressService.deleteAddress(addressId, receiverId);
+  }
+
+  protected isSelectedSetAsDefaultLoading(
+    addressId: number,
+    receiverId: number,
+  ): boolean {
+    return this.setAsDefaultAddressService.isSelectedLoading(
+      addressId,
+      receiverId,
+    );
+  }
+
+  protected isSelectedDeleteLoading(
+    addressId: number,
+    receiverId: number,
+  ): boolean {
+    return this.deleteAddressService.isSelectedLoading(addressId, receiverId);
+  }
+}
