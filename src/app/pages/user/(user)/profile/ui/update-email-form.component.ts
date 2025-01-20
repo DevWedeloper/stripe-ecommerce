b5470aa754d1bb83f318ab92e1e79b9a7d053425
DevWedeloper <vicnathangabrielle@gmail.com>
2025-01-20 20:50:34 +0800

    feat: create update email form component

---
+++ src/app/pages/user/(user)/profile/ui/update-email-form.component.ts
@@ -0,0 +1,69 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+  viewChild,
+} from '@angular/core';
+import { FormGroup, FormGroupDirective } from '@angular/forms';
+import { HlmCardImports } from '@spartan-ng/ui-card-helm';
+import {
+  authInputErrorProvider,
+  sharedFormDeps,
+} from 'src/app/shared/utils/form';
+
+@Component({
+  selector: 'app-update-email-form',
+  standalone: true,
+  imports: [...HlmCardImports, ...sharedFormDeps],
+  providers: [authInputErrorProvider],
+  template: `
+    <div hlmCard>
+      <div hlmCardHeader>
+        <h3 hlmCardTitle>Update your email</h3>
+        <p hlmCardDescription>
+          Update your email address to keep your account information current.
+        </p>
+      </div>
+
+      <form [formGroup]="form()" (ngSubmit)="onSubmit()">
+        <div hlmCardContent class="flex flex-col gap-4">
+          <hlm-form-field>
+            <label hlmLabel for="email">Email</label>
+            <input
+              hlmInput
+              formControlName="email"
+              id="email"
+              type="email"
+              placeholder="Email"
+              class="w-full"
+            />
+          </hlm-form-field>
+        </div>
+
+        <div hlmCardFooter class="flex justify-end">
+          <button
+            hlmBtnWithLoading
+            [disabled]="form().invalid || isLoading() || disableTemporarily()"
+            [isLoading]="isLoading()"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UpdateEmailFormComponent {
+  form = input.required<FormGroup>();
+  isLoading = input.required<boolean>();
+  disableTemporarily = input.required<boolean>();
+  submitChange = output<void>();
+
+  formDir = viewChild.required(FormGroupDirective);
+
+  protected onSubmit(): void {
+    this.submitChange.emit();
+  }
+}
