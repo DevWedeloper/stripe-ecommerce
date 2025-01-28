4ca39b9588021fa1f5b8ce5b8af4face0f223591
DevWedeloper <vicnathangabrielle@gmail.com>
2025-01-29 06:03:07 +0800

    feat: create delete user component

---
+++ src/app/pages/user/(user)/profile/feature/delete-user.component.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmCardImports } from '@spartan-ng/ui-card-helm';
+import { HlmDialogService } from '@spartan-ng/ui-dialog-helm';
+import { ConfirmDeleteAccountComponent } from './confirm-delete-account.component';
+
+@Component({
+  selector: 'app-delete-user',
+  standalone: true,
+  imports: [HlmButtonDirective, ...HlmCardImports],
+  template: `
+    <div hlmCard>
+      <div hlmCardHeader>
+        <h3 hlmCardTitle>Delete your account</h3>
+        <p hlmCardDescription>
+          Permanently remove your account and all associated data.
+        </p>
+      </div>
+
+      <div hlmCardFooter class="flex justify-end">
+        <button hlmBtn variant="destructive" (click)="onClick()">
+          Delete account
+        </button>
+      </div>
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DeleteUserComponent {
+  private _hlmDialogService = inject(HlmDialogService);
+
+  protected onClick(): void {
+    this._hlmDialogService.open(ConfirmDeleteAccountComponent, {
+      contentClass: 'flex',
+    });
+  }
+}
