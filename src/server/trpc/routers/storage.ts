d15476e2b785fbb1569f235e2d30a2f021b12c06
DevWedeloper <vicnathangabrielle@gmail.com>
2025-02-11 03:32:22 +0800

    feat: create createSignedUploadUrl procedure

--- src/server/trpc/routers/storage.ts
+++ src/server/trpc/routers/storage.ts
@@ -1,3 +1,23 @@
-import { router } from '../trpc';
+import { createSignedUploadUrl } from 'src/server/use-cases/storage/create-signed-upload-url';
+import { createClient } from 'src/supabase/server';
+import { z } from 'zod';
+import { publicProcedure, router } from '../trpc';
 
-export const storageRouter = router({});
+export const storageRouter = router({
+  createSignedUploadUrl: publicProcedure
+    .input(
+      z.object({
+        storageId: z.string(),
+        path: z.string(),
+        options: z
+          .object({
+            upsert: z.boolean(),
+          })
+          .optional(),
+      }),
+    )
+    .mutation(
+      async ({ input, ctx: { event } }) =>
+        await createSignedUploadUrl(createClient(event), input),
+    ),
+});
