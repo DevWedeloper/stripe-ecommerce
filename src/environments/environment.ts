6fef2c9e793ecb328f8eb146f87e133e5e6953d3
DevWedeloper <vicnathangabrielle@gmail.com>
2025-02-11 07:45:37 +0800

    feat: add productImagesS3Bucket

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,5 @@ import { getEnvVar } from 'src/env';
 export const environment = {
   s3Url: getEnvVar('VITE_S3_URL'),
   stripePublishableKey: getEnvVar('VITE_STRIPE_PUBLISHABLE_KEY'),
+  productImagesS3Bucket: getEnvVar('VITE_PRODUCT_IMAGES_S3_BUCKET'),
 };
