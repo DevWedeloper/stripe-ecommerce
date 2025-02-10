dd707ff51e9b71f70ec851d48b77685efe4ee54b
DevWedeloper <vicnathangabrielle@gmail.com>
2025-02-11 03:33:33 +0800

    feat: create updateStatus procedure

--- src/server/trpc/routers/orders.ts
+++ src/server/trpc/routers/orders.ts
@@ -1,6 +1,7 @@
 import { createSelectSchema } from 'drizzle-zod';
 import { orderStatusEnum } from 'src/db/schema';
 import { getOrdersByUserId } from 'src/server/use-cases/order/get-orders-by-user-id';
+import { updateOrderStatus } from 'src/server/use-cases/order/update-order-status';
 import { z } from 'zod';
 import { publicProcedure, router } from '../trpc';
 
@@ -17,5 +18,17 @@ export const ordersRouter = router({
     .query(
       async ({ input: { userId, page, pageSize, status } }) =>
         await getOrdersByUserId(userId, page, pageSize, status),
+    ),
+
+  updateStatus: publicProcedure
+    .input(
+      z.object({
+        orderId: z.number(),
+        status: createSelectSchema(orderStatusEnum),
+      }),
+    )
+    .mutation(
+      async ({ input: { orderId, status } }) =>
+        await updateOrderStatus(orderId, status),
     ),
 });
