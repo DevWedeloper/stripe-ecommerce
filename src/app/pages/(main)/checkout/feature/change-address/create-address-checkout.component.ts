093f0d0d5e082a1352aaf4bd5820db3bad263ef9
DevWedeloper <vicnathangabrielle@gmail.com>
2025-02-14 19:13:27 +0800

    feat: create address checkout component

---
+++ src/app/pages/(main)/checkout/feature/change-address/create-address-checkout.component.ts
@@ -0,0 +1,77 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  output,
+  viewChild,
+} from '@angular/core';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { FormBuilder, ValueChangeEvent } from '@angular/forms';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { filter } from 'rxjs';
+import { CreateAddressService } from 'src/app/shared/data-access/address/create-address.service';
+import { CountriesService } from 'src/app/shared/data-access/countries.service';
+import { AddressFormComponent } from 'src/app/shared/ui/address-form.component';
+import { initializeAddressForm } from 'src/app/shared/utils/form';
+import { disableTemporarilyStream } from 'src/app/shared/utils/rxjs';
+
+@Component({
+  selector: 'app-create-address-checkout',
+  imports: [AddressFormComponent, HlmButtonDirective],
+  template: `
+    <app-address-form
+      class="mb-4 block"
+      [form]="form"
+      [countries]="countries()"
+      [isLoading]="isLoading()"
+      [disableTemporarily]="disableTemporarily()"
+      (submitChange)="onSubmit()"
+    />
+    <button hlmBtn class="w-full" (click)="cancelChange.emit()">Cancel</button>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CreateAddressCheckoutComponent {
+  private fb = inject(FormBuilder);
+
+  private createAddressService = inject(CreateAddressService);
+  private countriesService = inject(CountriesService);
+
+  cancelChange = output<void>();
+
+  private success$ = this.createAddressService.createAddressSuccess$;
+  protected isLoading = this.createAddressService.isLoading;
+  private error$ = this.createAddressService.createAddressError$;
+
+  protected countries = this.countriesService.countries;
+
+  private addressForm = viewChild.required(AddressFormComponent);
+
+  protected form = initializeAddressForm(this.fb);
+
+  private disableTemporarily$ = disableTemporarilyStream({
+    enable: this.form.events.pipe(
+      filter((event) => event instanceof ValueChangeEvent),
+    ),
+    disable: this.error$,
+  });
+
+  protected disableTemporarily = toSignal(this.disableTemporarily$, {
+    initialValue: false,
+  });
+
+  constructor() {
+    this.success$
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.addressForm().formDir().resetForm());
+  }
+
+  protected onSubmit(): void {
+    if (!this.form.controls.countryId.value) return;
+
+    this.createAddressService.createAddress({
+      ...this.form.getRawValue(),
+      countryId: this.form.controls.countryId.value,
+    });
+  }
+}
