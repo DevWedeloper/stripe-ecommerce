511adbdd751834206fd5931cf7f3ee8186869151
DevWedeloper <vicnathangabrielle@gmail.com>
2025-02-20 11:04:33 +0800

    refactor: move getCategoryTree to /category

--- src/server/trpc/routers/categories.ts
+++ src/server/trpc/routers/categories.ts
@@ -1,4 +1,4 @@
-import { getCategoryTree } from 'src/server/use-cases/get-category-tree';
+import { getCategoryTree } from 'src/server/use-cases/category/get-category-tree';
 import { publicProcedure, router } from '../trpc';
 
 export const categoriesRouter = router({
