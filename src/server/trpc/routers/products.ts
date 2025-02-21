10e4b486f4c852df9314f79d90bb2eae32fb06a8
DevWedeloper <vicnathangabrielle@gmail.com>
2025-02-21 15:05:48 +0800

    feat: create create procedure

--- src/server/trpc/routers/products.ts
+++ src/server/trpc/routers/products.ts
@@ -1,4 +1,12 @@
+import { createInsertSchema } from 'drizzle-zod';
+import {
+  productItems,
+  products,
+  variationOptions,
+  variations,
+} from 'src/db/schema';
 import { positiveIntSchema } from 'src/schemas/zod-schemas';
+import { createProduct } from 'src/server/use-cases/product/create-product';
 import { getProductById } from 'src/server/use-cases/product/get-product-by-id';
 import { getProductsByCategoryName } from 'src/server/use-cases/product/get-products-by-category-name';
 import { searchProductsByKeyword } from 'src/server/use-cases/product/search-products-by-keyword';
@@ -6,6 +14,49 @@ import { z } from 'zod';
 import { publicProcedure, router } from '../trpc';
 
 export const productsRouter = router({
+  create: publicProcedure
+    .input(
+      z.object({
+        productData: createInsertSchema(products).omit({ isDeleted: true }),
+        variationsData: z.array(
+          createInsertSchema(variations).omit({ productId: true }),
+        ),
+        variationOptionsData: z.array(
+          createInsertSchema(variationOptions)
+            .omit({ variationId: true })
+            .merge(
+              z.object({
+                variationName: z.string(),
+              }),
+            ),
+        ),
+        productItemsData: z.array(
+          createInsertSchema(productItems)
+            .omit({ productId: true })
+            .merge(
+              z.object({
+                variations: z.array(
+                  z.object({
+                    variationName: z.string(),
+                    variationValue: z.string(),
+                  }),
+                ),
+              }),
+            ),
+        ),
+        categoryId: z.number(),
+        productImagesData: z.array(
+          z.object({
+            imagePath: z.string(),
+            placeholder: z.string(),
+            isThumbnail: z.boolean(),
+          }),
+        ),
+        tagIds: z.array(z.number()).optional(),
+      }),
+    )
+    .mutation(async ({ input }) => await createProduct(input)),
+
   getById: publicProcedure
     .input(positiveIntSchema)
     .query(async ({ input }) => await getProductById(input)),
