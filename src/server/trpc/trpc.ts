625adfabf4c94b751f343c1c15117cb817fea148
DevWedeloper <vicnathangabrielle@gmail.com>
2025-03-06 10:33:32 +0800

    feat: create protectedProcedure

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -1,10 +1,25 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC, TRPCError } from '@trpc/server';
+import { createClient } from 'src/supabase/server';
 import { Context } from './context';
 
 const t = initTRPC.context<Context>().create({});
-/**
- * Unprotected procedure
- **/
-export const publicProcedure = t.procedure;
+
 export const router = t.router;
 export const middleware = t.middleware;
+export const publicProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(
+  async ({ ctx: { event }, next }) => {
+    const supabase = createClient(event);
+
+    const { error } = await supabase.auth.getUser();
+
+    if (error) {
+      throw new TRPCError({
+        code: 'UNAUTHORIZED',
+        message: 'User must be authenticated',
+      });
+    }
+
+    return next();
+  },
+);
