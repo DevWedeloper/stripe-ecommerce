3f3d7557773d02efaaee97ed33ac03705c1951a5
DevWedeloper <vicnathangabrielle@gmail.com>
2025-03-12 19:38:20 +0800

    feat: create get order all status list service

---
+++ src/app/pages/user/(user)/order/data-access/get-order-all-status-list.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+import { BaseGetOrder } from './base-get-order';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GetOrderAllStatusListService extends BaseGetOrder {}
