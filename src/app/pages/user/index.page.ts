cdab7d7cf6857bd12f4c0d8b1ebba48d68e6230f
DevWedeloper <vicnathangabrielle@gmail.com>
2025-03-24 10:29:40 +0800

    feat: create user page

---
+++ src/app/pages/user/index.page.ts
@@ -0,0 +1,46 @@
+import { RouteMeta } from '@analogjs/router';
+import { Component } from '@angular/core';
+import { isAuthenticatedGuard } from 'src/app/shared/guards/is-authenticated.guard';
+import { SideNavComponent } from 'src/app/shared/layout/side-nav/side-nav.component';
+import { DeleteUserComponent } from './(user)/profile/feature/delete-user.component';
+import { UpdateAvatarComponent } from './(user)/profile/feature/update-avatar.component';
+import { UpdateEmailComponent } from './(user)/profile/feature/update-email.component';
+import { UpdatePasswordComponent } from './(user)/profile/feature/update-password.component';
+import { CloseButtonComponent } from './ui/close-button.component';
+
+export const routeMeta: RouteMeta = {
+  canActivate: [isAuthenticatedGuard],
+};
+
+@Component({
+  selector: 'app-account',
+  standalone: true,
+  imports: [
+    SideNavComponent,
+    CloseButtonComponent,
+    UpdateAvatarComponent,
+    UpdateEmailComponent,
+    UpdatePasswordComponent,
+    DeleteUserComponent,
+  ],
+  host: {
+    class: 'relative w-full flex justify-center',
+  },
+  template: `
+    <app-close-button />
+
+    <div class="flex w-full max-w-[1080px] flex-row gap-4">
+      <app-side-nav class="py-16" />
+
+      <main class="hidden w-full py-16 lg:block">
+        <div class="flex flex-col gap-4">
+          <app-update-avatar />
+          <app-update-email />
+          <app-update-password />
+          <app-delete-user />
+        </div>
+      </main>
+    </div>
+  `,
+})
+export default class AccountPageComponent {}
