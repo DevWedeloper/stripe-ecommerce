39abe0848a363513d61cbc11985282a8808b28b0
DevWedeloper <vicnathangabrielle@gmail.com>
2025-04-08 23:26:46 +0800

    fix: build is stuck

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
+import { getEnvVar } from 'src/env';
 
-const client = postgres(import.meta.env['DATABASE_URL']);
+const client = postgres(getEnvVar('DATABASE_URL'), {
+  prepare: false,
+});
 export const db = drizzle(client);
