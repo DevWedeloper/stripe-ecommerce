dc1a79b448be627dd2e3547cc5775885ace015f5
DevWedeloper <vicnathangabrielle@gmail.com>
2025-04-10 21:46:33 +0800

    fix: refetch on review mutation

--- src/app/pages/user/(user)/order/data-access/get-order-all-status-list.service.ts
+++ src/app/pages/user/(user)/order/data-access/get-order-all-status-list.service.ts
@@ -1,5 +1,16 @@
 import { Injectable } from '@angular/core';
+import { merge, Observable } from 'rxjs';
 import { BaseGetOrder } from './base-get-order';
 
 @Injectable()
-export class GetOrderAllStatusListService extends BaseGetOrder {}
+export class GetOrderAllStatusListService extends BaseGetOrder {
+  protected override get trigger$(): Observable<any> {
+    return merge(
+      this.filter$,
+      this.updateOrderStatusService.updateOrderStatusSuccess$,
+      this.createReviewService.createReviewSuccessWithData$,
+      this.updateReviewService.updateReviewSuccess$,
+      this.deleteReviewService.deleteReviewSuccess$,
+    );
+  }
+}
