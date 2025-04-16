fbff937cc16a8167952f9fb0184e55c041ff86e7
DevWedeloper <vicnathangabrielle@gmail.com>
2025-04-17 00:01:45 +0800

    refactor: remove unused code

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -8,7 +8,6 @@ const t = initTRPC.context<Context>().create({
 });
 
 export const router = t.router;
-export const middleware = t.middleware;
 export const publicProcedure = t.procedure;
 export const protectedProcedure = t.procedure.use(
   async ({ ctx: { event }, next }) => {
