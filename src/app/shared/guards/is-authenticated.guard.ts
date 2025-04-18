6180b4c6b23b779c13e0b36e6e80358a00044c64
DevWedeloper <vicnathangabrielle@gmail.com>
2025-04-18 08:50:07 +0800

    perf: execute getUser only once in server

--- src/app/shared/guards/is-authenticated.guard.ts
+++ src/app/shared/guards/is-authenticated.guard.ts
@@ -1,11 +1,19 @@
-import { inject } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { map } from 'rxjs';
 import { AuthService } from '../data-access/auth.service';
 
 export const isAuthenticatedGuard: CanActivateFn = () => {
   const router = inject(Router);
+  const platformId = inject(PLATFORM_ID);
   const authService = inject(AuthService);
+
+  if (isPlatformServer(platformId)) {
+    return authService.user$.pipe(
+      map((user) => (user ? true : router.parseUrl('/login'))),
+    );
+  }
 
   return authService
     .getUserAndSet$()
